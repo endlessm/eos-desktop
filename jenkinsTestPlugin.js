67c13c38b5576a30e4137a0dabf8a4cd816c50f3
sgnn7 <sgnn7@sgnn7.org>
2013-04-04 14:20:13 -0500

    Have correct jenkins output -Srdjan & Matt I

--- jenkinsTestPlugin.js
+++ jenkinsTestPlugin.js
@@ -1,6 +1,9 @@
 const INDENT_AMOUNT = 4;
 const TIME_RESOLUTION = 5;
 
+const CDATA_START = '<![CDATA[';
+const CDATA_END = ']]>';
+
 function _indent(text, indentationLevel){
     let indentationString = "";
     if(indentationLevel && indentationLevel > 0){
@@ -22,31 +25,56 @@ function _tag(tag_name, properties, indentationLevel){
         });
     }
 
-    return _indent("<" + tag_name + propertyString + ">\n", indentationLevel);
+    return _indent("<" + tag_name + propertyString + ">", indentationLevel);
 }
 
 function _formatTime(timeTaken){
-    roundedTime = timeTaken * Math.pow(10, TIME_RESOLUTION);
+    let roundedTime = timeTaken * Math.pow(10, TIME_RESOLUTION);
     roundedTime = Math.round(roundedTime);
  
     return "" + roundedTime / Math.pow(10, TIME_RESOLUTION);
 }
 
-function outputJenkinsResults(filename, classname, results){
+function _errorNode(error) {
+    const ERROR_NODE='error';
+    let properties = { 'type': error.type,
+                       'message': error.message };
+
+    let errorNodeString = _tag(ERROR_NODE, properties, 2);
+    errorNodeString += CDATA_START + error.stackTrace + CDATA_END;
+    errorNodeString += _closingTag(ERROR_NODE);
+
+    return errorNodeString;
+}
+
+function outputJenkinsResults(filename, aggregateResults){
     const XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     const ROOT_NODE = "testsuite";
     const TESTCASE = "testcase";
+    const LINE_BREAK = "\n";
 
     let xmlContent = XML_HEADER;
-    xmlContent += _tag(ROOT_NODE, { 'name': classname, 'tests': 123, 'errors': 123, 'failures': 0, 'skip':0} );
-    
-    results.forEach(function(result){
-        let testClass = classname + "." + result.name;
-        xmlContent += _tag(TESTCASE, { 'classname': testClass, 'name':result.name, 'time':_formatTime(result.time) }, 1) + _closingTag(TESTCASE, 1);
+    let properties = { 'name': aggregateResults.name, 
+                       'tests': aggregateResults.tests, 
+                       'errors': aggregateResults.errors, 
+                       'failures': aggregateResults.failures, 
+                       'skip': aggregateResults.skip };
+    xmlContent += _tag(ROOT_NODE, properties) + LINE_BREAK;
+
+    aggregateResults.results.forEach(function(result){
+        let properties = { 'classname': aggregateResults.name + "." + result.name,
+                           'name':result.name, 
+                           'time':_formatTime(result.time) };
+
+        xmlContent += _tag(TESTCASE, properties, 1) + LINE_BREAK;
+        if ("error" in result) {
+            xmlContent += _errorNode(result.error);
+        }
+        xmlContent += _closingTag(TESTCASE, 1);
     });
     xmlContent += _closingTag(ROOT_NODE);
     
-    print(xmlContent);
+    if (this.DEBUG) print(xmlContent);
 
     const Gio = imports.gi.Gio;
     let outputFile = Gio.file_new_for_path(filename);
