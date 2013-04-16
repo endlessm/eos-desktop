8591adb0f324e4eb345a3eeda5b3f118452aee8d
Matt Irwin <irwinmb14@yahoo.com>
2013-04-16 14:58:47 -0500

    removed fade in and out when dragging an app icon - Erik and Matt [endlessm/eos-shell#67]

--- js/ui/overviewControls.js
+++ js/ui/overviewControls.js
@@ -496,20 +496,6 @@ const ControlsManager = new Lang.Class({
         this._viewSelector.connect('page-empty', Lang.bind(this, this._onPageEmpty));
 
         Main.overview.connect('showing', Lang.bind(this, this._updateSpacerVisibility));
-        Main.overview.connect('item-drag-begin', Lang.bind(this,
-            function() {
-                let activePage = this._viewSelector.getActivePage();
-                if (activePage != ViewSelector.ViewPage.WINDOWS)
-                    this._viewSelector.fadeHalf();
-            }));
-        Main.overview.connect('item-drag-end', Lang.bind(this,
-            function() {
-                this._viewSelector.fadeIn();
-            }));
-        Main.overview.connect('item-drag-cancelled', Lang.bind(this,
-            function() {
-                this._viewSelector.fadeIn();
-            }));
     },
 
     _setVisibility: function() {
