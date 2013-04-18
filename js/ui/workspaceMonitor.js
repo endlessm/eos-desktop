3285a886b72111a42997f9606633127040c4ecdc
Iain Holmes <iain@sleepfive.com>
2013-04-18 15:16:16 +0100

    Listen to window destroyed signal instead of window removed
    window-destroyed is the opposite of map, not window-removed.
    Get the list of windows open at startup and work out if they're visible or
    minimised
    [endlessm/eos-shell#23]

--- js/ui/workspaceMonitor.js
+++ js/ui/workspaceMonitor.js
@@ -13,6 +13,8 @@ const WorkspaceMonitor = new Lang.Class({
         this._shellwm = global.window_manager;
         this._shellwm.connect('minimize', Lang.bind(this, this._minimizeWindow));
         this._shellwm.connect('map', Lang.bind(this, this._mapWindow));
+        this._shellwm.connect('destroy', Lang.bind(this, this._destroyWindow));
+
         this._metaScreen.connect('workspace-switched', Lang.bind(this, this._workspaceSwitched));
 
         this._visibleWindows = 0;
@@ -33,19 +35,21 @@ const WorkspaceMonitor = new Lang.Class({
         // When we switch workspace or on startup we need to track what
         // windows already exist on the system. Currently we only have
         // one workspace so that isn't a problem.
+
         // The code below works for all windows, except gnome-terminal
         //  - for some reason gnome-terminal windows do not appear in
         // the list returned by list_windows().
-        // Also there doesn't appear to be a method
-        // (that works at startup time) to check if a window is on screen
-        // or minimized
-        /*
         let windows = this._activeWorkspace.list_windows();
         global.log("Got " + windows.length + " windows");
         for (let i = 0; i < windows.length; i++) {
             let metaWindow = windows[i];
+
+            if (metaWindow.minimized) {
+                this._minimizedWindows.push(metaWindow);
+            } else {
+                this._visibleWindows += 1;
+            }
         }
-        */
 
         this.updateOverview();
     },
@@ -78,10 +82,13 @@ const WorkspaceMonitor = new Lang.Class({
     },
 
     _windowRemoved: function(metaWorkspace, metaWindow) {
+    },
+
+    _destroyWindow: function(shellwm, actor) {
         // _windowRemoved is called for both visible and minimized windows
         // so we only need to care about tracking it if it
         // hasn't been minimized
-        let idx = this._minimizedWindows.indexOf(metaWindow);
+        let idx = this._minimizedWindows.indexOf(actor.meta_window);
         if (idx == -1) {
             this._visibleWindows -= 1;
         } else {
@@ -115,9 +122,13 @@ const WorkspaceMonitor = new Lang.Class({
 
     updateOverview: function() {
         if (this._visibleWindows == 0) {
-            Main.overview.show();
+            if (!Main.overview.visible) {
+                Main.overview.show();
+            }
         } else {
-            Main.overview.hide();
+            if (Main.overview.visible) {
+                Main.overview.hide();
+            }
         }
     }
 });
