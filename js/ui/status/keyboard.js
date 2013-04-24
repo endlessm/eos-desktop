541e8b530cecacf1f53c9c909cab589497eaeec0
Giovanni Campagna <gcampagna@src.gnome.org>
2013-04-24 21:38:00 +0200

    Keyboard: clear currentSource after destruction
    StLabel doesn't like that we set its properties after destructions,
    and this would happen in currentInputSourceChanged() at the end,
    when setting the ornament.

--- js/ui/status/keyboard.js
+++ js/ui/status/keyboard.js
@@ -458,6 +458,7 @@ const InputSourceIndicator = new Lang.Class({
 
         this._inputSources = {};
         this._ibusSources = {};
+        this._currentSource = null;
 
         let inputSourcesByShortName = {};
 
