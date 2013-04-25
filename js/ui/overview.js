4ef454a4ae3cd48b3e116b3aa82b2bb80894ca53
Roddy Shuler <roddy@endlessm.com>
2013-04-25 09:47:49 -0700

    Reinstate the dash and workspace thumbnails that were hidden by stories #9 and #10; we will want these for the window picker, so we should hide them only for the application selector [endlessm/eos-shell#176]

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -264,12 +264,6 @@ const Overview = new Lang.Class({
                                                     expand: true });
         this._group.add_actor(this._controls.thumbnailsActor);
 
-        // Hide the dash
-        this._controls.dashActor.hide();
-
-        // Hide the workspace thumbnails
-        this._controls.thumbnailsActor.hide();
-
         // Add our same-line elements after the search entry
         this._overview.add(this._groupStack, { y_fill: true, expand: true });
 
