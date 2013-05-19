f54990fcdeddf04851380c15707388978f4777c2
Jasper St. Pierre <jstpierre@mecheye.net>
2013-05-18 20:59:46 -0400

    layout: Remove affectsInputRegion
    This can easy be worked around by adding things to the uiGroup
    instead, so there's really no reason to have it here still.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=700735

--- js/ui/layout.js
+++ js/ui/layout.js
@@ -162,7 +162,6 @@ const MonitorConstraint = new Lang.Class({
 const defaultParams = {
     trackFullscreen: false,
     affectsStruts: false,
-    affectsInputRegion: true
 };
 
 const LayoutManager = new Lang.Class({
@@ -783,11 +782,10 @@ const LayoutManager = new Lang.Class({
     // @actor: an actor to add to the chrome
     // @params: (optional) additional params
     //
-    // Adds @actor to the chrome, and (unless %affectsInputRegion in
-    // @params is %false) extends the input region to include it.
-    // Changes in @actor's size, position, and visibility will
-    // automatically result in appropriate changes to the input
-    // region.
+    // Adds @actor to the chrome, and extends the input region
+    // to include it. Changes in @actor's size, position, and
+    // visibility will automatically result in appropriate changes
+    // to the input region.
     //
     // If %affectsStruts in @params is %true (and @actor is along a
     // screen edge), then @actor's size and position will also affect
@@ -1006,7 +1004,7 @@ const LayoutManager = new Lang.Class({
 
         for (i = 0; i < this._trackedActors.length; i++) {
             let actorData = this._trackedActors[i];
-            if (!(actorData.affectsInputRegion && wantsInputRegion) && !actorData.affectsStruts)
+            if (!wantsInputRegion && !actorData.affectsStruts)
                 continue;
 
             let [x, y] = actorData.actor.get_transformed_position();
@@ -1016,7 +1014,7 @@ const LayoutManager = new Lang.Class({
             w = Math.round(w);
             h = Math.round(h);
 
-            if (actorData.affectsInputRegion && wantsInputRegion && actorData.actor.get_paint_visibility())
+            if (wantsInputRegion && actorData.actor.get_paint_visibility())
                 rects.push(new Meta.Rectangle({ x: x, y: y, width: w, height: h }));
 
             if (actorData.affectsStruts) {
