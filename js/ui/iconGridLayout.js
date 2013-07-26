c7769650fbd856d44315220b5d8c542cb867a6c9
Emmanuele Bassi <ebassi@gnome.org>
2013-07-26 16:11:16 +0100

    Correctly invoke repositionIcon from convenience methods
    Use the right magic arguments for appending and removing an icon.
    
    [endlessm/eos-shell#635]

--- js/ui/iconGridLayout.js
+++ js/ui/iconGridLayout.js
@@ -53,23 +53,11 @@ const IconGridLayout = new Lang.Class({
     },
 
     appendIcon: function(id) {
-        this.repositionIcon(id, -1, null);
+        this.repositionIcon(id, null, '');
     },
 
     removeIcon: function(id) {
-        let icons;
-        for (let i in this._iconTree) {
-            icon = this._iconTree[i];
-            let oldPos = icons.indexOf(id);
-            if (oldPos != -1) {
-                icons.splice(oldPos, 1);
-                break;
-            }
-        }
-
-        let newLayout = GLib.Variant.new("a{sas}", this._iconTree);
-
-        global.settings.set_value(SCHEMA_KEY, newLayout);
+        this.repositionIcon(id, 0, null);
     },
 
     repositionIcon: function(id, insertId, newFolder) {
