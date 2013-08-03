endlessm/eos-desktop

commit 3797662f90bdfb924cb24bc450294f07c9d02bbd

Use appendIcon() when possible
Instead of repositionIcon() with magic numbers again.

[endlessm/eos-shell#635]

js/ui/appDisplay.js
@@ -54,6 +54,7 @@ const SPLASH_SCREEN_FADE_OUT = 0.2;
 const SPLASH_SCREEN_COMPLETE_TIME = 250;
 
 const EOS_APP_STORE_ID = 'eos-app-store.desktop';
+const ALL_VIEW_ID = '';
 
 const EndlessApplicationView = new Lang.Class({
     Name: 'EndlessApplicationView',
@@ -655,7 +656,7 @@ const AllView = new Lang.Class({
     },
 
     getViewId: function() {
-        return '';
+        return ALL_VIEW_ID;
     },
 
     _appStoreWindowsChanged: function() {
@@ -1069,7 +1070,7 @@ const FolderIcon = new Lang.Class({
 
     handleIconDrop: function(source) {
         // Move the source icon into this folder
-        IconGridLayout.layout.repositionIcon(source.getId(), null, this.getId());
+        IconGridLayout.layout.appendIcon(source.getId(), null, this.getId());
         return true;
     },
 

js/ui/iconGridLayout.js
@@ -52,8 +52,8 @@ const IconGridLayout = new Lang.Class({
         return id && this._iconTree[id];
     },
 
-    appendIcon: function(id) {
-        this.repositionIcon(id, null, '');
+    appendIcon: function(id, folderId) {
+        this.repositionIcon(id, null, folderId);
     },
 
     removeIcon: function(id) {

js/ui/shellDBus.js
@@ -6,6 +6,7 @@ const Lang = imports.lang;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 
+const AppDisplay = imports.ui.appDisplay;
 const Config = imports.misc.config;
 const ExtensionSystem = imports.ui.extensionSystem;
 const ExtensionDownloader = imports.ui.extensionDownloader;
@@ -427,7 +428,7 @@ const AppStoreService = new Lang.Class({
     },
 
     AddApplication: function(id) {
-        IconGridLayout.layout.appendIcon(id);
+        IconGridLayout.layout.appendIcon(id, AppDisplay.ALL_VIEW_ID);
     },
 
     RemoveApplication: function(id) {

commit 18fedf801a931c97ae7d0558b84e1d64bdfda2f0

Fix bug dropping icon into folder
This was introduced by commit 3797662f90bdfb924cb24bc450294f07c9d02bbd.

[endlessm/eos-shell#717]

js/ui/appDisplay.js
@@ -1079,7 +1079,7 @@ const FolderIcon = new Lang.Class({
 
     handleIconDrop: function(source) {
         // Move the source icon into this folder
-        IconGridLayout.layout.appendIcon(source.getId(), null, this.getId());
+        IconGridLayout.layout.appendIcon(source.getId(), this.getId());
         return true;
     },
 