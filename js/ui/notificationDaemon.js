b50047e75821619a75344b5e33962987d8c7c25f
Jasper St. Pierre <jstpierre@mecheye.net>
2013-10-13 10:44:28 -0400

    notificationDaemon: Rename the existing implementation to the FdoNotificationSource
    We'll add a new, simpler private implementation that's used by the new
    GNotification API in gio.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=710137

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -31,7 +31,7 @@ function Bus() {
     return new BusProxy(Gio.DBus.session, 'org.freedesktop.DBus', '/org/freedesktop/DBus');
 }
 
-const NotificationDaemonIface = '<node> \
+const FdoNotificationsIface = '<node> \
 <interface name="org.freedesktop.Notifications"> \
 <method name="Notify"> \
     <arg type="s" direction="in"/> \
@@ -106,11 +106,11 @@ const STANDARD_TRAY_ICON_IMPLEMENTATIONS = {
     'ibus-ui-gtk': 'keyboard'
 };
 
-const NotificationDaemon = new Lang.Class({
-    Name: 'NotificationDaemon',
+const FdoNotificationDaemon = new Lang.Class({
+    Name: 'FdoNotificationDaemon',
 
     _init: function() {
-        this._dbusImpl = Gio.DBusExportedObject.wrapJSObject(NotificationDaemonIface, this);
+        this._dbusImpl = Gio.DBusExportedObject.wrapJSObject(FdoNotificationsIface, this);
         this._dbusImpl.export(Gio.DBus.session, '/org/freedesktop/Notifications');
 
         this._sources = [];
@@ -218,7 +218,7 @@ const NotificationDaemon = new Lang.Class({
             }
         }
 
-        let source = new Source(title, pid, sender, trayIcon, ndata ? ndata.hints['desktop-entry'] : null);
+        let source = new FdoNotificationDaemonSource(title, pid, sender, trayIcon, ndata ? ndata.hints['desktop-entry'] : null);
         source.setTransient(isForTransientNotification);
 
         if (!isForTransientNotification) {
@@ -546,8 +546,8 @@ const NotificationDaemon = new Lang.Class({
     }
 });
 
-const Source = new Lang.Class({
-    Name: 'NotificationDaemonSource',
+const FdoNotificationDaemonSource = new Lang.Class({
+    Name: 'FdoNotificationDaemonSource',
     Extends: MessageTray.Source,
 
     _init: function(title, pid, sender, trayIcon, appId) {
@@ -714,4 +714,12 @@ const Source = new Lang.Class({
             return null;
         }
     }
+});
+
+const NotificationDaemon = new Lang.Class({
+    Name: 'NotificationDaemon',
+
+    _init: function() {
+        this._fdoNotificationDaemon = new FdoNotificationDaemon();
+    },
 });
