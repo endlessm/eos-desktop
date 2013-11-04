700b3bb95e4acc398958cafd6b66395a1ccffc57
Jasper St. Pierre <jstpierre@mecheye.net>
2013-11-04 11:45:22 -0500

    notificationDaemon: Only store policies for "real" apps
    Fake, window-backed apps should not have a policy for them.

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -583,7 +583,7 @@ const FdoNotificationDaemonSource = new Lang.Class({
     },
 
     _createPolicy: function() {
-        if (this.app) {
+        if (this.app && this.app.get_app_info()) {
             let id = this.app.get_id().replace(/\.desktop$/,'');
             return new MessageTray.NotificationApplicationPolicy(id);
         } else {
