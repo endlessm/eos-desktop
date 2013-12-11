78382dcb6b4f1e859f760db0078f1754df0a81ea
Sam Spilsbury <smspillaz@gmail.com>
2013-12-11 10:42:38 -0800

    Rewrite jsParse tests to use test fixtures

--- tests/unit/jsParse.js
+++ tests/unit/jsParse.js
@@ -2,100 +2,13 @@
 
 // Test cases for MessageTray URLification
 
+const Lang = imports.lang;
 const JsUnit = imports.jsUnit;
 
 const Environment = imports.ui.environment;
 Environment.init();
 
 const JsParse = imports.misc.jsParse;
-
-const HARNESS_COMMAND_HEADER = "let imports = obj;" +
-                               "let global = obj;" +
-                               "let Main = obj;" +
-                               "let foo = obj;" +
-                               "let r = obj;";
-
-const testsFindMatchingQuote = [
-    { input: '"double quotes"',
-      output: 0 },
-    { input: '\'single quotes\'',
-      output: 0 },
-    { input: 'some unquoted "some quoted"',
-      output: 14 },
-    { input: '"mixed \' quotes\'"',
-      output: 0 },
-    { input: '"escaped \\" quote"',
-      output: 0 }
-];
-const testsFindMatchingSlash = [
-    { input: '/slash/',
-      output: 0 },
-    { input: '/slash " with $ funny ^\' stuff/',
-      output: 0  },
-    { input: 'some unslashed /some slashed/',
-      output: 15 },
-    { input: '/escaped \\/ slash/',
-      output: 0 }
-];
-const testsFindMatchingBrace = [
-    { input: '[square brace]',
-      output: 0 },
-    { input: '(round brace)',
-      output: 0  },
-    { input: '([()][nesting!])',
-      output: 0 },
-    { input: '[we have "quoted [" braces]',
-      output: 0 },
-    { input: '[we have /regex [/ braces]',
-      output: 0 },
-    { input: '([[])[] mismatched braces ]',
-      output: 1 }
-];
-const testsGetExpressionOffset = [
-    { input: 'abc.123',
-      output: 0 },
-    { input: 'foo().bar',
-      output: 0  },
-    { input: 'foo(bar',
-      output: 4 },
-    { input: 'foo[abc.match(/"/)]',
-      output: 0 }
-];
-const testsGetDeclaredConstants = [
-    { input: 'const foo = X; const bar = Y;',
-      output: ['foo', 'bar'] },
-    { input: 'const foo=X; const bar=Y',
-      output: ['foo', 'bar'] }
-];
-const testsIsUnsafeExpression = [
-    { input: 'foo.bar',
-      output: false },
-    { input: 'foo[\'bar\']',
-      output: false  },
-    { input: 'foo["a=b=c".match(/=/)',
-      output: false },
-    { input: 'foo[1==2]',
-      output: false },
-    { input: '(x=4)',
-      output: true },
-    { input: '(x = 4)',
-      output: true },
-    { input: '(x;y)',
-      output: true }
-];
-const testsModifyScope = [
-    "foo['a",
-    "foo()['b'",
-    "obj.foo()('a', 1, 2, 'b')().",
-    "foo.[.",
-    "foo]]]()))].",
-    "123'ab\"",
-    "Main.foo.bar = 3; bar.",
-    "(Main.foo = 3).",
-    "Main[Main.foo+=-1]."
-];
-
-
 
 // Utility function for comparing arrays
 function assertArrayEquals(errorMessage, array1, array2) {
@@ -110,85 +23,213 @@ function assertArrayEquals(errorMessage, array1, array2) {
 //
 // Test javascript parsing
 //
+//
+// TODO: We probably want to change all these to use
+//       a table driven method, using for() inside
+//       a test body hampers readibility and
+//       debuggability when something goes wrong.
 
-for (let i = 0; i < testsFindMatchingQuote.length; i++) {
-    let text = testsFindMatchingQuote[i].input;
-    let match = JsParse.findMatchingQuote(text, text.length - 1);
+function testParseForMatchingQuoteForEndOfString() {
+    const testsFindMatchingQuote = [
+        { input: '"double quotes"',
+        output: 0 },
+        { input: '\'single quotes\'',
+        output: 0 },
+        { input: 'some unquoted "some quoted"',
+        output: 14 },
+        { input: '"mixed \' quotes\'"',
+          output: 0 },
+        { input: '"escaped \\" quote"',
+          output: 0 }
+    ];
 
-    JsUnit.assertEquals('Test testsFindMatchingQuote ' + i,
-			match, testsFindMatchingQuote[i].output);
+    for (let i = 0; i < testsFindMatchingQuote.length; i++) {
+        let text = testsFindMatchingQuote[i].input;
+        let match = JsParse.findMatchingQuote(text, text.length - 1);
+        let expected = testsFindMatchingQuote[i].output;
+
+        JsUnit.assertEquals("Matching quote not found at position " + expected,
+                            match, expected);
+    }
 }
 
-for (let i = 0; i < testsFindMatchingSlash.length; i++) {
-    let text = testsFindMatchingSlash[i].input;
-    let match = JsParse.findMatchingSlash(text, text.length - 1);
 
-    JsUnit.assertEquals('Test testsFindMatchingSlash ' + i,
-			match, testsFindMatchingSlash[i].output);
+function testParseForMatchingSlashAtEndOfString() {
+    const testsFindMatchingSlash = [
+        { input: '/slash/',
+          output: 0 },
+        { input: '/slash " with $ funny ^\' stuff/',
+          output: 0  },
+        { input: 'some unslashed /some slashed/',
+          output: 15 },
+        { input: '/escaped \\/ slash/',
+          output: 0 }
+    ];
+
+    for (let i = 0; i < testsFindMatchingSlash.length; i++) {
+        let text = testsFindMatchingSlash[i].input;
+        let match = JsParse.findMatchingSlash(text, text.length - 1);
+        let expected = testsFindMatchingSlash[i].output;
+
+        JsUnit.assertEquals("Matching slash not found at position " + expected,
+                            match, expected);
+    }
 }
 
-for (let i = 0; i < testsFindMatchingBrace.length; i++) {
-    let text = testsFindMatchingBrace[i].input;
-    let match = JsParse.findMatchingBrace(text, text.length - 1);
+function testParseForMatchingBraceAtEndOfString() {
+    const testsFindMatchingBrace = [
+        { input: '[square brace]',
+          output: 0 },
+        { input: '(round brace)',
+          output: 0  },
+        { input: '([()][nesting!])',
+          output: 0 },
+        { input: '[we have "quoted [" braces]',
+          output: 0 },
+        { input: '[we have /regex [/ braces]',
+          output: 0 },
+        { input: '([[])[] mismatched braces ]',
+          output: 1 }
+    ];
 
-    JsUnit.assertEquals('Test testsFindMatchingBrace ' + i,
-			match, testsFindMatchingBrace[i].output);
+    for (let i = 0; i < testsFindMatchingBrace.length; i++) {
+        let text = testsFindMatchingBrace[i].input;
+        let match = JsParse.findMatchingBrace(text, text.length - 1);
+        let expected = testsFindMatchingBrace[i].output;
+
+        JsUnit.assertEquals("Matching brace not found at position " + expected,
+                            match, expected);
+    }
 }
 
-for (let i = 0; i < testsGetExpressionOffset.length; i++) {
-    let text = testsGetExpressionOffset[i].input;
-    let match = JsParse.getExpressionOffset(text, text.length - 1);
+function testSearchForBeginningOfExpression() {
+    const testsGetExpressionOffset = [
+        { input: 'abc.123',
+          output: 0 },
+        { input: 'foo().bar',
+          output: 0  },
+        { input: 'foo(bar',
+          output: 4 },
+        { input: 'foo[abc.match(/"/)]',
+          output: 0 }
+    ];
 
-    JsUnit.assertEquals('Test testsGetExpressionOffset ' + i,
-			match, testsGetExpressionOffset[i].output);
+    for (let i = 0; i < testsGetExpressionOffset.length; i++) {
+        let text = testsGetExpressionOffset[i].input;
+        let match = JsParse.getExpressionOffset(text, text.length - 1);
+        let expected = testsGetExpressionOffset[i].output;
+
+        JsUnit.assertEquals("Beginning of expression matches was not at " + expected,
+                            match, expected);
+    }
 }
 
-for (let i = 0; i < testsGetDeclaredConstants.length; i++) {
-    let text = testsGetDeclaredConstants[i].input;
-    let match = JsParse.getDeclaredConstants(text);
+function testFindConstantVariableIdentifiersInExpression() {
+    const testsGetDeclaredConstants = [
+        { input: 'const foo = X; const bar = Y;',
+          output: ['foo', 'bar'] },
+        { input: 'const foo=X; const bar=Y',
+          output: ['foo', 'bar'] }
+    ];
 
-    assertArrayEquals('Test testsGetDeclaredConstants ' + i,
-		      match, testsGetDeclaredConstants[i].output);
+    for (let i = 0; i < testsGetDeclaredConstants.length; i++) {
+        let text = testsGetDeclaredConstants[i].input;
+        let match = JsParse.getDeclaredConstants(text);
+        let expected = testsGetDeclaredConstants[i].output;
+        
+        let message = "Expected to find the following constants :\n";
+        let messageReference = { m: message };
+        let closure = Lang.bind(this, function(item) {
+            this.messageReference += "= " + item + "\n";
+        });
+        
+        expected.forEach(closure);
+        
+        assertArrayEquals(messageReference.m,
+                          match, expected);
+    }
 }
 
-for (let i = 0; i < testsIsUnsafeExpression.length; i++) {
-    let text = testsIsUnsafeExpression[i].input;
-    let unsafe = JsParse.isUnsafeExpression(text);
+function testIsUnsafeExpression() {
+    const testsIsUnsafeExpression = [
+        { input: 'foo.bar',
+          output: false },
+        { input: 'foo[\'bar\']',
+          output: false  },
+        { input: 'foo["a=b=c".match(/=/)',
+          output: false },
+        { input: 'foo[1==2]',
+          output: false },
+        { input: '(x=4)',
+          output: true },
+        { input: '(x = 4)',
+          output: true },
+        { input: '(x;y)',
+          output: true }
+    ];
 
-    JsUnit.assertEquals('Test testsIsUnsafeExpression ' + i,
-			unsafe, testsIsUnsafeExpression[i].output);
+    for (let i = 0; i < testsIsUnsafeExpression.length; i++) {
+        let text = testsIsUnsafeExpression[i].input;
+        let unsafe = JsParse.isUnsafeExpression(text);
+        let expected = testsIsUnsafeExpression[i].output;
+        
+        let message = "Expected that parser found that the expression " +
+                      text + " was " + (expected == true ? "unsafe" : "safe");
+
+        JsUnit.assertEquals(message,
+                            unsafe, testsIsUnsafeExpression[i].output);
+    }
 }
 
 //
 // Test safety of eval to get completions
 //
+function testSafetyOfEval() {
+    const HARNESS_COMMAND_HEADER = "let imports = obj;" +
+                                   "let global = obj;" +
+                                   "let Main = obj;" +
+                                   "let foo = obj;" +
+                                   "let r = obj;";
 
-for (let i = 0; i < testsModifyScope.length; i++) {
-    let text = testsModifyScope[i];
-    // We need to use var here for the with statement
-    var obj = {};
+    const testsModifyScope = [
+        "foo['a",
+        "foo()['b'",
+        "obj.foo()('a', 1, 2, 'b')().",
+        "foo.[.",
+        "foo]]]()))].",
+        "123'ab\"",
+        "Main.foo.bar = 3; bar.",
+        "(Main.foo = 3).",
+        "Main[Main.foo+=-1]."
+    ];
 
-    // Just as in JsParse.getCompletions, we will find the offset
-    // of the expression, test whether it is unsafe, and then eval it.
-    let offset = JsParse.getExpressionOffset(text, text.length - 1);
-    if (offset >= 0) {
-        text = text.slice(offset);
+    for (let i = 0; i < testsModifyScope.length; i++) {
+        let text = testsModifyScope[i];
+        // We need to use var here for the with statement
+        var obj = {};
 
-        let matches = text.match(/(.*)\.(.*)/);
-        if (matches) {
-            [expr, base, attrHead] = matches;
+        // Just as in JsParse.getCompletions, we will find the offset
+        // of the expression, test whether it is unsafe, and then eval it.
+        let offset = JsParse.getExpressionOffset(text, text.length - 1);
+        if (offset >= 0) {
+            text = text.slice(offset);
 
-            if (!JsParse.isUnsafeExpression(base)) {
-                with (obj) {
-                    try {
-                        eval(HARNESS_COMMAND_HEADER + base);
-                    } catch (e) {
-                        JsUnit.assertNotEquals("Code '" + base + "' is valid code", e.constructor, SyntaxError);
+            let matches = text.match(/(.*)\.(.*)/);
+            if (matches) {
+                [expr, base, attrHead] = matches;
+
+                if (!JsParse.isUnsafeExpression(base)) {
+                    with (obj) {
+                        try {
+                            eval(HARNESS_COMMAND_HEADER + base);
+                        } catch (e) {
+                            JsUnit.assertNotEquals("Code '" + base + "' is valid code", e.constructor, SyntaxError);
+                        }
                     }
-                }
+               }
            }
-       }
+        }
+        let propertyNames = Object.getOwnPropertyNames(obj);
+        JsUnit.assertEquals("The context '" + JSON.stringify(obj) + "' was not modified", propertyNames.length, 0);
     }
-    let propertyNames = Object.getOwnPropertyNames(obj);
-    JsUnit.assertEquals("The context '" + JSON.stringify(obj) + "' was not modified", propertyNames.length, 0);
 }
