933fd7e808b212197537ea2a9d41fb9a17ed6715
Sam Spilsbury <smspillaz@gmail.com>
2013-12-11 10:39:26 -0800

    Rewrite format.js tests to use test fixtures

--- tests/unit/format.js
+++ tests/unit/format.js
@@ -9,28 +9,64 @@ const assertEquals = JsUnit.assertEquals;
 const assertRaises = JsUnit.assertRaises;
 
 // We can't depend on environment.js to set up the String.prototype.format,
-// because the tests  run in one JS context, and the imports run in the GJS
+// because the tests run in one JS context, and the imports run in the GJS
 // "load context" which has its own copy of the String class
-const Format = imports.misc.format;
+const Format = imports.format;
 String.prototype.format = Format.format;
 
 // Test common usage and %% handling
-assertEquals("foo", "%s".format('foo'));
-assertEquals("%s", "%%s".format('foo'));
-assertEquals("%%s", "%%%%s".format('foo'));
-assertEquals("foo 5", "%s %d".format('foo', 5));
-assertEquals("8", "%d".format(8));
-assertEquals("f", "%x".format(15));
-assertEquals("2.58 6.96", "%f %.2f".format(2.58, 6.958));
+
+function testInsertStringFormat() {
+    assertEquals("foo", "%s".format('foo'));
+}
+
+function testInsertStringFormatTakesPrecedence() {
+    assertEquals("%s", "%%s".format('foo'));
+}
+
+function testInsertStringFormatTakesPrecedenceButFormatCharacterIsReplaced() {
+    assertEquals("%%s", "%%%%s".format('foo'));
+}
+
+function testInsertStringIntegerFormat() {
+    assertEquals("foo 5", "%s %d".format('foo', 5));
+}
+
+function testInsertIntegerFormat() {
+    assertEquals("8", "%d".format(8));
+}
+
+function testInsertAsciiFormat() {
+    assertEquals("f", "%x".format(15));
+}
+
+function testInsertFloatingPointPrecision() {
+    assertEquals("2.58 6.96", "%f %.2f".format(2.58, 6.958));
+}
 
 // Test field width
-assertEquals("007  foo", "%03d %4s".format(7, 'foo'));
-assertEquals(" 2.58 06.96", "%5f %05.2f".format(2.58, 6.958));
-assertEquals("cafe", "%2x".format(0xcafe));
-assertEquals("foo", "%0s".format('foo'));
+function testFieldWidthIntegerString() {
+    assertEquals("007  foo", "%03d %4s".format(7, 'foo'));
+}
+
+function testFieldWidthFloatingPoint() {
+    assertEquals(" 2.58 06.96", "%5f %05.2f".format(2.58, 6.958));
+}
+
+function testFieldWidthStripHex() {
+    assertEquals("cafe", "%2x".format(0xcafe));
+}
+
+function testFieldWidthNoStripCharacters() {
+    assertEquals("foo", "%0s".format('foo'));
+}
 
 // Precision is only allowed for %f
-assertRaises(function() { "%.2d".format(5.21) });
+function testRaisesOnAttemptToUsePrecisionForIntegers() {
+    assertRaises(function() { "%.2d".format(5.21) });
+}
 
 // Wrong conversion character ' '
-assertRaises( function() { "%s is 50% done".format('foo') });
+function testRaisesOnInvalidConversionCharacter() {
+    assertRaises( function() { "%s is 50% done".format('foo') });
+}
