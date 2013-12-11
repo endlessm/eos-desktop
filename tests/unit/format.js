5830577c6a74af3d0ff58147f06718200189d496
Sam Spilsbury <smspillaz@gmail.com>
2013-12-11 06:15:58 -0800

    Move to jasmine for the format tests

--- tests/unit/format.js
+++ tests/unit/format.js
@@ -8,62 +8,57 @@ const JsUnit = imports.jsUnit;
 const assertEquals = JsUnit.assertEquals;
 const assertRaises = JsUnit.assertRaises;
 
-const Environment = imports.misc.coreEnvironment;
+const CoreEnvironment = imports.misc.coreEnvironment;
 const Format = imports.format;
 
 // Test common usage and %% handling
 
-function testInsertStringFormat() {
-    assertEquals("foo", "%s".format('foo'));
-}
+describe ('String Format', function () {
+    beforeEach(function() {
+        CoreEnvironment.coreInit();
+    });
 
-function testInsertStringFormatTakesPrecedence() {
-    assertEquals("%s", "%%s".format('foo'));
-}
-
-function testInsertStringFormatTakesPrecedenceButFormatCharacterIsReplaced() {
-    assertEquals("%%s", "%%%%s".format('foo'));
-}
-
-function testInsertStringIntegerFormat() {
-    assertEquals("foo 5", "%s %d".format('foo', 5));
-}
-
-function testInsertIntegerFormat() {
-    assertEquals("8", "%d".format(8));
-}
-
-function testInsertAsciiFormat() {
-    assertEquals("f", "%x".format(15));
-}
-
-function testInsertFloatingPointPrecision() {
-    assertEquals("2.58 6.96", "%f %.2f".format(2.58, 6.958));
-}
-
-// Test field width
-function testFieldWidthIntegerString() {
-    assertEquals("007  foo", "%03d %4s".format(7, 'foo'));
-}
-
-function testFieldWidthFloatingPoint() {
-    assertEquals(" 2.58 06.96", "%5f %05.2f".format(2.58, 6.958));
-}
-
-function testFieldWidthStripHex() {
-    assertEquals("cafe", "%2x".format(0xcafe));
-}
-
-function testFieldWidthNoStripCharacters() {
-    assertEquals("foo", "%0s".format('foo'));
-}
-
-// Precision is only allowed for %f
-function testRaisesOnAttemptToUsePrecisionForIntegers() {
-    assertRaises(function() { "%.2d".format(5.21) });
-}
-
-// Wrong conversion character ' '
-function testRaisesOnInvalidConversionCharacter() {
-    assertRaises( function() { "%s is 50% done".format('foo') });
-}
+    it ('can insert simple strings', function () {
+        expect ('%s'.format ('foo')).toMatch ('foo');
+    });
+    it ('does not insert a string when % preceeds %s', function () {
+        expect ('%%s'.format ('foo')).toMatch ('%s');
+    });
+    it ('replaces %% with %', function () {
+        expect ('%%%%s'.format ('foo')).toMatch ('%%s');
+    });
+    it ('inserts strings and integers', function () {
+        expect ('%s %d'.format ('foo', 5)).toMatch ('foo 5');
+    });
+    it ('inserts integers', function () {
+        expect ('%d'.format (8)).toMatch ('8');
+    });
+    it ('inserts ascii format codes', function () {
+        expect ('%x'.format (15)).toMatch ('f');
+    });
+    it ('inserts floating point numbers with precision', function () {
+        expect ('%f %.2f'.format (2.58, 6.958)).toMatch ('2.58 6.96');
+    });
+    it ('inserts integers and strings with field with', function () {
+        expect ('%03d %4s'.format (7, 'foo')).toMatch ('007  foo');
+    });
+    it ('inserts floating point number with a field with', function () {
+        expect ('%5f %05.2f'.format (2.58, 6.958)).toMatch (' 2.58 06.96');
+    });
+    it ('strips 0x from hex numbers', function () {
+        expect ('%2x'.format (0xcafe)).toMatch ('cafe');
+    });
+    it ('does not strip characters even if width is zero', function () {
+        expect ('%0s'.format ('foo')).toMatch ('foo');
+    });
+    it ('throws an exception if we attempt to use precision for integers', function () {
+        expect (function () {
+            '%2.d'.format (5.21);
+        }).toThrow ();
+    });
+    it ('throws an exception on the wrong conversion character', function () {
+        expect (function () {
+                    '%s is 50% done'.format('foo');
+                }).toThrow ();
+    });
+});
