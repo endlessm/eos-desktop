061a321f79561f7ae501b0acbdc75c478b57f8ec
Sam Spilsbury <smspillaz@gmail.com>
2013-12-12 04:33:42 -0800

    Move insertSorted tests to jasmine.

--- tests/unit/insertSorted.js
+++ tests/unit/insertSorted.js
@@ -1,66 +1,6 @@
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
-
-// Test cases for Util.insertSorted
-
-const JsUnit = imports.jsUnit;
 const Util = imports.misc.util;
-
-const Environment = imports.misc.coreEnvironment;
-Environment.coreInit();
-
-function assertArrayEquals(array1, array2) {
-    JsUnit.assertEquals('Array lengths are not equal',
-                        array1.length, array2.length);
-    for (let j = 0; j < array1.length; j++) {
-        JsUnit.assertEquals('Array item ' + j + ' is not equal',
-                            array1[j], array2[j]);
-    }
-}
-
-function cmp(one, two) {
-    return one - two;
-}
-
-function testInsertIntegerWithComparator() {
-    let arrayInt = [1, 2, 3, 5, 6];
-    Util.insertSorted(arrayInt, 4, function(one, two) {
-                                       return one - two
-                                   });
-    assertArrayEquals([1, 2, 3, 4, 5, 6], arrayInt);
-}
-
-// no comparator, integer sorting is implied
-function testInsertIntegerWithoutComparator() {
-    let arrayInt = [1, 2, 3, 4, 5, 6];
-    Util.insertSorted(arrayInt, 3);
-    assertArrayEquals([1, 2, 3, 3, 4, 5, 6], arrayInt);
-}
-
-function testInsertObjectWithPropertyComparator() {
-    let obj1 = { a: 1 };
-    let obj2 = { a: 2, b: 0 };
-    let obj3 = { a: 2, b: 1 };
-    let obj4 = { a: 3 };
-
-    let arrayObj = [obj1, obj3, obj4];
-
-    // obj2 compares equivalent to obj3, should be
-    // inserted before
-    Util.insertSorted(arrayObj, obj2, function(one, two) {
-                                          return one.a - two.a
-                                      });
-    assertArrayEquals([obj1, obj2, obj3, obj4], arrayObj);
-}
-
-// check that no comparisons are made when
-// inserting in a empty array
-function testInsertingIntoEmptyArrayDoesNotCallComparator() {
-    let emptyArray = [];
-    Util.insertSorted(emptyArray, 3, function() {
-                                         throw "Comparator should not be called"
-                                     });
-}
-
+const CoreEnvironment = imports.misc.coreEnvironment;
 
 /* If we access past the end of an array, then the types of
  * either one or two will not be a number and this comparator
@@ -76,17 +16,55 @@ function checkedIntegerCmp(one, two) {
     return one - two;
 }
 
-// Insert at the end and check that we don't
-// access past it
-function testInsertingAndEndOfArrayDoesNotCauseInvalidAccess() {
-    let array = [1];
-    Util.insertSorted(array, 4, checkedIntegerCmp);
-    Util.insertSorted(array, 5, checkedIntegerCmp);
-}
+describe('Sorted Inserter Utility', function() {
+    beforeEach(function() {
+        CoreEnvironment.coreInit();
+    });
+    it('inserts sorted integers with an integer comparator', function() {
+        let arrayInt = [1, 2, 3, 5, 6];
+        Util.insertSorted(arrayInt, 4, function(one, two) {
+                                           return one - two
+                                       });
+        expect(arrayInt).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+    it('inserts sorted integers where there is no comparator', function() {
+        let arrayInt = [1, 2, 3, 4, 5, 6];
+        Util.insertSorted(arrayInt, 3);
+        expect(arrayInt).toEqual([1, 2, 3, 3, 4, 5, 6]);
+    });
+    it('inserts objects with property comparators', function() {
+        let obj1 = { a: 1 };
+        let obj2 = { a: 2, b: 0 };
+        let obj3 = { a: 2, b: 1 };
+        let obj4 = { a: 3 };
 
+        let arrayObj = [obj1, obj3, obj4];
 
-function testInsertingAtBeginningOfArrayDoesNotCauseInvalidAccess() {
-    let array = [1, 4, 5];
-    Util.insertSorted(array, 2, checkedIntegerCmp);
-    Util.insertSorted(array, 1, checkedIntegerCmp);
-}
+        // obj2 compares equivalent to obj3, should be
+        // inserted before
+        Util.insertSorted(arrayObj, obj2, function(one, two) {
+                                              return one.a - two.a
+                                          });
+        expect(arrayObj).toEqual([obj1, obj2, obj3, obj4]);
+    });
+    it('does not call the comparator when inserting into an empty array', function() {
+        let comparator = jasmine.createSpy ('Comparator Spy');
+        let emptyArray = [];
+        Util.insertSorted(emptyArray, 3, comparator);
+        expect(comparator).not.toHaveBeenCalled();
+    });
+    it('does not invalidly accesss the array when we insert at the end of the array', function() {
+        let array = [1];
+        expect(function() {
+                   Util.insertSorted(array, 4, checkedIntegerCmp);
+                   Util.insertSorted(array, 5, checkedIntegerCmp);
+               }).not.toThrow();
+    });
+    it('does not invalidly access the array when we insert the the beginning of the array', function() {
+        let array = [1, 4, 5];
+        expect(function() {
+                   Util.insertSorted(array, 2, checkedIntegerCmp);
+                   Util.insertSorted(array, 3, checkedIntegerCmp);
+               }).not.toThrow();
+    });
+});
