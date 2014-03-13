35329cde594741ab1b6e48ef3dd71850c7cc454e
Jasper St. Pierre <jstpierre@mecheye.net>
2014-03-13 18:51:10 -0400

    Implement window menus in gnome-shell
    https://bugzilla.gnome.org/show_bug.cgi?id=726352
    
    Conflicts:
    	js/ui/windowManager.js

--- src/gnome-shell-plugin.c
+++ src/gnome-shell-plugin.c
@@ -69,7 +69,8 @@ static void gnome_shell_plugin_show_tile_preview (MetaPlugin      *plugin,
                                                   MetaRectangle   *tile_rect,
                                                   int              tile_monitor);
 static void gnome_shell_plugin_hide_tile_preview (MetaPlugin *plugin);
-
+static void gnome_shell_plugin_show_window_menu  (MetaPlugin *plugin,
+                                                  MetaWindow *window);
 
 static gboolean              gnome_shell_plugin_xevent_filter (MetaPlugin *plugin,
                                                                XEvent     *event);
@@ -136,6 +137,7 @@ gnome_shell_plugin_class_init (GnomeShellPluginClass *klass)
 
   plugin_class->show_tile_preview = gnome_shell_plugin_show_tile_preview;
   plugin_class->hide_tile_preview = gnome_shell_plugin_hide_tile_preview;
+  plugin_class->show_window_menu = gnome_shell_plugin_show_window_menu;
 
   plugin_class->xevent_filter     = gnome_shell_plugin_xevent_filter;
   plugin_class->keybinding_filter = gnome_shell_plugin_keybinding_filter;
@@ -331,6 +333,13 @@ static void
 gnome_shell_plugin_hide_tile_preview (MetaPlugin *plugin)
 {
   _shell_wm_hide_tile_preview (get_shell_wm ());
+}
+
+static void
+gnome_shell_plugin_show_window_menu (MetaPlugin *plugin,
+                                     MetaWindow *window)
+{
+  _shell_wm_show_window_menu (get_shell_wm (), window);
 }
 
 static gboolean
