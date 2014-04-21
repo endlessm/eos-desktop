GNOME/gnome-shell

Stringify the xml definitions for E4X removal
https://bugzilla.gnome.org/show_bug.cgi?id=691409

js/ui/notificationDaemon.js
@@ -16,51 +16,55 @@ const Params = imports.misc.params;
 const Util = imports.misc.util;
 
 // Should really be defined in Gio.js
-const BusIface = <interface name="org.freedesktop.DBus">
-<method name="GetConnectionUnixProcessID">
-    <arg type="s" direction="in" />
-    <arg type="u" direction="out" />
-</method>
-</interface>;
+const BusIface = '<node> \
+<interface name="org.freedesktop.DBus"> \
+<method name="GetConnectionUnixProcessID"> \
+    <arg type="s" direction="in" /> \
+    <arg type="u" direction="out" /> \
+</method> \
+</interface> \
+</node>';
 
 var BusProxy = Gio.DBusProxy.makeProxyWrapper(BusIface);
 function Bus() {
     return new BusProxy(Gio.DBus.session, 'org.freedesktop.DBus', '/org/freedesktop/DBus');
 }
 
-const FdoNotificationsIface = <interface name="org.freedesktop.Notifications">
-<method name="Notify">
-    <arg type="s" direction="in"/>
-    <arg type="u" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="as" direction="in"/>
-    <arg type="a{sv}" direction="in"/>
-    <arg type="i" direction="in"/>
-    <arg type="u" direction="out"/>
-</method>
-<method name="CloseNotification">
-    <arg type="u" direction="in"/>
-</method>
-<method name="GetCapabilities">
-    <arg type="as" direction="out"/>
-</method>
-<method name="GetServerInformation">
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-</method>
-<signal name="NotificationClosed">
-    <arg type="u"/>
-    <arg type="u"/>
-</signal>
-<signal name="ActionInvoked">
-    <arg type="u"/>
-    <arg type="s"/>
-</signal>
-</interface>;
+const FdoNotificationsIface = '<node> \
+<interface name="org.freedesktop.Notifications"> \
+<method name="Notify"> \
+    <arg type="s" direction="in"/> \
+    <arg type="u" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="as" direction="in"/> \
+    <arg type="a{sv}" direction="in"/> \
+    <arg type="i" direction="in"/> \
+    <arg type="u" direction="out"/> \
+</method> \
+<method name="CloseNotification"> \
+    <arg type="u" direction="in"/> \
+</method> \
+<method name="GetCapabilities"> \
+    <arg type="as" direction="out"/> \
+</method> \
+<method name="GetServerInformation"> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+</method> \
+<signal name="NotificationClosed"> \
+    <arg type="u"/> \
+    <arg type="u"/> \
+</signal> \
+<signal name="ActionInvoked"> \
+    <arg type="u"/> \
+    <arg type="s"/> \
+</signal> \
+</interface> \
+</node>';
 
 const NotificationClosedReason = {
     EXPIRED: 1,
@@ -767,16 +771,18 @@ const GtkNotificationDaemonNotification = new Lang.Class({
     },
 });
 
-const FdoApplicationIface = <interface name="org.freedesktop.Application">
-<method name="ActivateAction">
-    <arg type="s" direction="in" />
-    <arg type="av" direction="in" />
-    <arg type="a{sv}" direction="in" />
-</method>
-<method name="Activate">
-    <arg type="a{sv}" direction="in" />
-</method>
-</interface>;
+const FdoApplicationIface = '<node> \
+<interface name="org.freedesktop.Application"> \
+<method name="ActivateAction"> \
+    <arg type="s" direction="in" /> \
+    <arg type="av" direction="in" /> \
+    <arg type="a{sv}" direction="in" /> \
+</method> \
+<method name="Activate"> \
+    <arg type="a{sv}" direction="in" /> \
+</method> \
+</interface> \
+</node>';
 const FdoApplicationProxy = Gio.DBusProxy.makeProxyWrapper(FdoApplicationIface);
 
 function objectPathFromAppId(appId) {
@@ -860,17 +866,19 @@ const GtkNotificationDaemonAppSource = new Lang.Class({
     },
 });
 
-const GtkNotificationsIface = <interface name="org.gtk.Notifications">
-<method name="AddNotification">
-    <arg type="s" direction="in" />
-    <arg type="s" direction="in" />
-    <arg type="a{sv}" direction="in" />
-</method>
-<method name="RemoveNotification">
-    <arg type="s" direction="in" />
-    <arg type="s" direction="in" />
-</method>
-</interface>;
+const GtkNotificationsIface = '<node> \
+<interface name="org.gtk.Notifications"> \
+<method name="AddNotification"> \
+    <arg type="s" direction="in" /> \
+    <arg type="s" direction="in" /> \
+    <arg type="a{sv}" direction="in" /> \
+</method> \
+<method name="RemoveNotification"> \
+    <arg type="s" direction="in" /> \
+    <arg type="s" direction="in" /> \
+</method> \
+</interface> \
+</node>';
 
 const GtkNotificationDaemon = new Lang.Class({
     Name: 'GtkNotificationDaemon',

js/ui/status/power.js
@@ -11,15 +11,17 @@ const PopupMenu = imports.ui.popupMenu;
 const BUS_NAME = 'org.freedesktop.UPower';
 const OBJECT_PATH = '/org/freedesktop/UPower/devices/DisplayDevice';
 
-const DisplayDeviceInterface = <interface name="org.freedesktop.UPower.Device">
-  <property name="Type" type="u" access="read"/>
-  <property name="State" type="u" access="read"/>
-  <property name="Percentage" type="d" access="read"/>
-  <property name="TimeToEmpty" type="x" access="read"/>
-  <property name="TimeToFull" type="x" access="read"/>
-  <property name="IsPresent" type="b" access="read"/>
-  <property name="IconName" type="s" access="read"/>
-</interface>;
+const DisplayDeviceInterface = '<node> \
+<interface name="org.freedesktop.UPower.Device"> \
+  <property name="Type" type="u" access="read"/> \
+  <property name="State" type="u" access="read"/> \
+  <property name="Percentage" type="d" access="read"/> \
+  <property name="TimeToEmpty" type="x" access="read"/> \
+  <property name="TimeToFull" type="x" access="read"/> \
+  <property name="IsPresent" type="b" access="read"/> \
+  <property name="IconName" type="s" access="read"/> \
+</interface> \
+</node>';
 
 const PowerManagerProxy = Gio.DBusProxy.makeProxyWrapper(DisplayDeviceInterface);
 

endlessm/eos-desktop

Conflicts:
	js/gdm/oVirt.js
	js/misc/loginManager.js
	js/misc/objectManager.js
	js/misc/smartcardManager.js
	js/ui/endSessionDialog.js
	js/ui/magnifierDBus.js
	js/ui/notificationDaemon.js
	js/ui/shellDBus.js
	js/ui/status/brightness.js
	js/ui/status/keyboard.js
	js/ui/status/power.js
	js/ui/status/rfkill.js

js/ui/notificationDaemon.js
@@ -19,51 +19,55 @@ const Util = imports.misc.util;
 let nextNotificationId = 1;
 
 // Should really be defined in Gio.js
-const BusIface = <interface name="org.freedesktop.DBus">
-<method name="GetConnectionUnixProcessID">
-    <arg type="s" direction="in" />
-    <arg type="u" direction="out" />
-</method>
-</interface>;
+const BusIface = '<node> \
+<interface name="org.freedesktop.DBus"> \
+<method name="GetConnectionUnixProcessID"> \
+    <arg type="s" direction="in" /> \
+    <arg type="u" direction="out" /> \
+</method> \
+</interface> \
+</node>';
 
 var BusProxy = Gio.DBusProxy.makeProxyWrapper(BusIface);
 function Bus() {
     return new BusProxy(Gio.DBus.session, 'org.freedesktop.DBus', '/org/freedesktop/DBus');
 }
 
-const NotificationDaemonIface = <interface name="org.freedesktop.Notifications">
-<method name="Notify">
-    <arg type="s" direction="in"/>
-    <arg type="u" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="s" direction="in"/>
-    <arg type="as" direction="in"/>
-    <arg type="a{sv}" direction="in"/>
-    <arg type="i" direction="in"/>
-    <arg type="u" direction="out"/>
-</method>
-<method name="CloseNotification">
-    <arg type="u" direction="in"/>
-</method>
-<method name="GetCapabilities">
-    <arg type="as" direction="out"/>
-</method>
-<method name="GetServerInformation">
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-    <arg type="s" direction="out"/>
-</method>
-<signal name="NotificationClosed">
-    <arg type="u"/>
-    <arg type="u"/>
-</signal>
-<signal name="ActionInvoked">
-    <arg type="u"/>
-    <arg type="s"/>
-</signal>
-</interface>;
+const NotificationDaemonIface = '<node> \
+<interface name="org.freedesktop.Notifications"> \
+<method name="Notify"> \
+    <arg type="s" direction="in"/> \
+    <arg type="u" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="s" direction="in"/> \
+    <arg type="as" direction="in"/> \
+    <arg type="a{sv}" direction="in"/> \
+    <arg type="i" direction="in"/> \
+    <arg type="u" direction="out"/> \
+</method> \
+<method name="CloseNotification"> \
+    <arg type="u" direction="in"/> \
+</method> \
+<method name="GetCapabilities"> \
+    <arg type="as" direction="out"/> \
+</method> \
+<method name="GetServerInformation"> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+    <arg type="s" direction="out"/> \
+</method> \
+<signal name="NotificationClosed"> \
+    <arg type="u"/> \
+    <arg type="u"/> \
+</signal> \
+<signal name="ActionInvoked"> \
+    <arg type="u"/> \
+    <arg type="s"/> \
+</signal> \
+</interface> \
+</node>';
 
 const NotificationClosedReason = {
     EXPIRED: 1,

js/ui/status/power.js
@@ -35,15 +35,17 @@ const UPDeviceState = {
     PENDING_DISCHARGE: 6
 };
 
-const PowerManagerInterface = <interface name="org.gnome.SettingsDaemon.Power">
-<method name="GetDevices">
-    <arg type="a(susdut)" direction="out" />
-</method>
-<method name="GetPrimaryDevice">
-    <arg type="(susdut)" direction="out" />
-</method>
-<property name="Icon" type="s" access="read" />
-</interface>;
+const PowerManagerInterface = '<node> \
+<interface name="org.gnome.SettingsDaemon.Power"> \
+<method name="GetDevices"> \
+    <arg type="a(susdut)" direction="out" /> \
+</method> \
+<method name="GetPrimaryDevice"> \
+    <arg type="(susdut)" direction="out" /> \
+</method> \
+<property name="Icon" type="s" access="read" /> \
+</interface> \
+</node>';
 
 const PowerManagerProxy = Gio.DBusProxy.makeProxyWrapper(PowerManagerInterface);
 