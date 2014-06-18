endlessm/eos-desktop

commit ebb391b1d00912280cb1e2c62b6b08e683f22508

Replace X-Endless-SplashScreen by X-Endless-LaunchMaximized
The later does the same as the former, plus it does not maximize the
window.

[endlessm/eos-shell#2752]

js/ui/appActivation.js
@@ -24,7 +24,7 @@ const SPLASH_SCREEN_TIMEOUT = 700; // ms
 const SPLASH_SCREEN_FADE_OUT = 0.2;
 const SPLASH_SCREEN_COMPLETE_TIME = 0.2;
 
-const SPLASH_SCREEN_DESKTOP_KEY = 'X-Endless-SplashScreen';
+const LAUNCH_MAXIMIZED_DESKTOP_KEY = 'X-Endless-LaunchMaximized';
 const SPLASH_BACKGROUND_DESKTOP_KEY = 'X-Endless-SplashBackground';
 const DEFAULT_SPLASH_SCREEN_BACKGROUND = global.datadir + '/theme/splash-background-default.jpg';
 const SPINNER_IMAGES_DIR = global.datadir + '/theme/';
@@ -56,10 +56,10 @@ const AppActivationContext = new Lang.Class({
     },
 
     showSplash: function() {
-        // Don't show splash screen if the splash screen key is false
+        // Don't show splash screen if the launch maximized key is false
         let info = this._app.get_app_info();
 
-        if (info && info.has_key(SPLASH_SCREEN_DESKTOP_KEY) && !info.get_boolean(SPLASH_SCREEN_DESKTOP_KEY)) {
+        if (info && info.has_key(LAUNCH_MAXIMIZED_DESKTOP_KEY) && !info.get_boolean(LAUNCH_MAXIMIZED_DESKTOP_KEY)) {
             return;
         }
 
@@ -449,6 +449,13 @@ const DesktopAppClient = new Lang.Class({
     },
 
     _windowCreated: function(metaDisplay, metaWindow) {
+        // Don't maximize if the launch maximized key is false
+        let info = this._app.get_app_info();
+
+        if (info && info.has_key(LAUNCH_MAXIMIZED_DESKTOP_KEY) && !info.get_boolean(LAUNCH_MAXIMIZED_DESKTOP_KEY)) {
+            return;
+        }
+
         // Don't maximize if key to disable default maximize is set
         if (global.settings.get_boolean(WindowManager.NO_DEFAULT_MAXIMIZE_KEY)) {
             return;

data/eos-app-feedback.desktop.in.in
@@ -6,6 +6,6 @@ Type=Application
 Exec=chromium-browser chrome-extension://kilnnmemaannmgjifgmiifljomfeaaig/index.html#feedback
 Icon=
 Categories=Utility;
-X-Endless-Splash-Screen=false
+X-Endless-LaunchMaximized=false
 X-Endless-ShowInAppStore=false
 

data/eos-app-tutorial.desktop.in.in
@@ -6,6 +6,6 @@ Type=Application
 Exec=gnome-welcome-tour
 Icon=
 Categories=Utility;
-X-Endless-Splash-Screen=false
+X-Endless-LaunchMaximized=false
 X-Endless-ShowInAppStore=false
 

commit 3680e7c0511ecfd1f3bc45b0aad04f8a8efba41e

Fix bug introduced in ebb391b1d.
[endlessm/eos-shell#2752]

js/ui/appActivation.js
@@ -449,13 +449,6 @@ const DesktopAppClient = new Lang.Class({
     },
 
     _windowCreated: function(metaDisplay, metaWindow) {
-        // Don't maximize if the launch maximized key is false
-        let info = this._app.get_app_info();
-
-        if (info && info.has_key(LAUNCH_MAXIMIZED_DESKTOP_KEY) && !info.get_boolean(LAUNCH_MAXIMIZED_DESKTOP_KEY)) {
-            return;
-        }
-
         // Don't maximize if key to disable default maximize is set
         if (global.settings.get_boolean(WindowManager.NO_DEFAULT_MAXIMIZE_KEY)) {
             return;
@@ -468,6 +461,13 @@ const DesktopAppClient = new Lang.Class({
         let tracker = Shell.WindowTracker.get_default();
         let app = tracker.get_window_app(metaWindow);
         if (!app) {
+            return;
+        }
+
+        // Don't maximize if the launch maximized key is false
+        let info = app.get_app_info();
+
+        if (info && info.has_key(LAUNCH_MAXIMIZED_DESKTOP_KEY) && !info.get_boolean(LAUNCH_MAXIMIZED_DESKTOP_KEY)) {
             return;
         }
 