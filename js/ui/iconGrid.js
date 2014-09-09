endlessm/eos-desktop

commit 7cb1a263989f31160e83315e6b9573a45c68fb24

iconGridLayout: animate icon undeletion
Instead of keeping the whole previous layout
so it can be restored when the user uses "Undo", just keep
the position of the removed item and use it to reposition it
in its previous place.

appDisplay: fix _findIconChanges() so it lists removed items.

iconGrid: split out the code calculating a child's position
from _allocate(). When new icons are going to be inserted in
the grid, we use this _childAllocation() method to calculate
the target positions of existing icons that need to be
animated as they move into new slots.

[endlessm/eos-shell#2086]

js/ui/iconGrid.js
@@ -549,30 +549,13 @@ const IconGrid = new Lang.Class({
                 leftPadding = availWidth - usedWidth;
         }
 
-        let x = box.x1 + leftPadding;
-        let y = box.y1;
-        let columnIndex = 0;
-        let rowIndex = 0;
+        // Store some information about the allocated layout
+        this._leftPadding = leftPadding;
+        this._allocatedColumns = nColumns;
+
         for (let i = 0; i < children.length; i++) {
-            let [childMinWidth, childMinHeight, childNaturalWidth, childNaturalHeight]
-                = children[i].get_preferred_size();
-
-            /* Center the item in its allocation horizontally */
-            let width = Math.max(this._hItemSize, childNaturalWidth);
-            let childXSpacing = Math.max(0, width - childNaturalWidth) / 2;
-            let height = Math.max(this._vItemSize, childNaturalHeight);
-            let childYSpacing = Math.max(0, height - childNaturalHeight) / 2;
-
-            let childBox = new Clutter.ActorBox();
-            if (Clutter.get_default_text_direction() == Clutter.TextDirection.RTL) {
-                let _x = box.x2 - (x + width);
-                childBox.x1 = Math.floor(_x - childXSpacing);
-            } else {
-                childBox.x1 = Math.floor(x + childXSpacing);
-            }
-            childBox.y1 = Math.floor(y + childYSpacing);
-            childBox.x2 = childBox.x1 + childNaturalWidth;
-            childBox.y2 = childBox.y1 + childNaturalHeight;
+            let rowIndex = Math.floor((i + 1) / this._allocatedColumns);
+            let childBox = this._childAllocation(children[i], box, i);
 
             if (this._rowLimit && rowIndex >= this._rowLimit ||
                 this._fillParent && childBox.y2 > availHeight) {
@@ -581,24 +564,36 @@ const IconGrid = new Lang.Class({
                 children[i].allocate(childBox, flags);
                 this.actor.set_skip_paint(children[i], false);
             }
-
-            columnIndex++;
-            if (columnIndex == nColumns) {
-                columnIndex = 0;
-                rowIndex++;
-            }
-
-            if (columnIndex == 0) {
-                y += this._vItemSize + this._spacing;
-                x = box.x1 + leftPadding;
-            } else {
-                x += this._hItemSize + this._spacing;
-            }
         }
+    },
 
-        // Store some information about the allocated layout
-        this._leftPadding = leftPadding;
-        this._allocatedColumns = nColumns;
+    _childAllocation: function(child, box, index) {
+        let [childMinWidth, childMinHeight, childNaturalWidth, childNaturalHeight]
+            = child.get_preferred_size();
+
+        let column = index % this._allocatedColumns;
+        let row = Math.floor(index / this._allocatedColumns);
+        let x = box.x1 + this._leftPadding + column * (this._hItemSize + this._spacing);
+        let y = box.y1 + row * (this._vItemSize + this._spacing);
+
+        /* Center the item in its allocation horizontally */
+        let width = Math.max(this._hItemSize, childNaturalWidth);
+        let childXSpacing = Math.max(0, width - childNaturalWidth) / 2;
+        let height = Math.max(this._vItemSize, childNaturalHeight);
+        let childYSpacing = Math.max(0, height - childNaturalHeight) / 2;
+
+        let childBox = new Clutter.ActorBox();
+        if (Clutter.get_default_text_direction() == Clutter.TextDirection.RTL) {
+            let _x = box.x2 - (x + width);
+            childBox.x1 = Math.floor(_x - childXSpacing);
+        } else {
+            childBox.x1 = Math.floor(x + childXSpacing);
+        }
+        childBox.y1 = Math.floor(y + childYSpacing);
+        childBox.x2 = childBox.x1 + childNaturalWidth;
+        childBox.y2 = childBox.y1 + childNaturalHeight;
+
+        return childBox;
     },
 
     childrenInRow: function(rowWidth) {
@@ -683,17 +678,26 @@ const IconGrid = new Lang.Class({
         this.actor.paint();
 
         let children = this.actor.get_children();
+        let node = this.actor.get_theme_node();
+        let contentBox = node.get_content_box(this.actor.allocation);
 
         let movementMatrix = {};
         // Find out where icons need to move
         for (let sourceIndex in changedItems) {
             let targetIndex = changedItems[sourceIndex];
-            if (targetIndex < children.length) {
-                movementMatrix[sourceIndex] = this._findActorOffset(children[sourceIndex], children[targetIndex]);
+            let sourceActor = children[sourceIndex];
+            let actorOffset;
+
+            if (targetIndex >= children.length) {
+                // calculate the position of the new slot
+                let oldBox = sourceActor.allocation;
+                let newBox = this._childAllocation(sourceActor, contentBox, targetIndex);
+                actorOffset = [newBox.x1 - oldBox.x1, newBox.y1 - oldBox.y1];
             } else {
-                delete changedItems[sourceIndex];
-                removedItems.push(sourceIndex);
+                actorOffset = this._findActorOffset(sourceActor, children[targetIndex]);
             }
+
+            movementMatrix[sourceIndex] = actorOffset;
         }
 
         // Make the original icon look like it fell into its place

js/ui/appDisplay.js
@@ -235,12 +235,12 @@ const EndlessApplicationView = new Lang.Class({
             let oldItem = oldItemLayout[oldItemIdx];
             let newItemIdx = newItemLayout.indexOf(oldItem);
 
-            // Did this icon move?
-            if (newItemIdx != -1 && oldItemIdx != newItemIdx) {
-                movedList[oldItemIdx] = newItemIdx;
-            // Did it get removed?
-            } else if (newItemIdx == -1) {
-                removedList.push(oldItemIdx);
+            if (oldItemIdx != newItemIdx) {
+                if (newItemIdx < 0) {
+                    removedList.push(oldItemIdx);
+                } else {
+                    movedList[oldItemIdx] = newItemIdx;
+                }
             }
         }
 

js/ui/iconGridLayout.js
@@ -30,7 +30,6 @@ const IconGridLayout = new Lang.Class({
         this._updateIconTree();
 
         this._removeUndone = false;
-        this._previousLayout = null;
 
         global.settings.connect('changed::' + SCHEMA_KEY, Lang.bind(this, function() {
             this._updateIconTree();
@@ -117,14 +116,36 @@ const IconGridLayout = new Lang.Class({
     },
 
     hasIcon: function(id) {
-        for (let folderIdx in this._iconTree) {
-            let folder = this._iconTree[folderIdx];
+        for (let folderId in this._iconTree) {
+            let folder = this._iconTree[folderId];
             if (folder.indexOf(id) != -1) {
                 return true;
             }
         }
 
         return false;
+    },
+
+    _getIconLocation: function(id) {
+        for (let folderId in this._iconTree) {
+            let folder = this._iconTree[folderId];
+            let nIcons = folder.length;
+
+            let itemIdx = folder.indexOf(id);
+            let nextId;
+
+            if (itemIdx < nIcons) {
+                nextId = folder[itemIdx + 1];
+            } else {
+                // append to the folder
+                nextId = null;
+            }
+
+            if (itemIdx != -1) {
+                return [folderId, nextId];
+            }
+        }
+        return null;
     },
 
     getIcons: function(folder) {
@@ -148,8 +169,15 @@ const IconGridLayout = new Lang.Class({
             return;
         }
 
-        this._previousLayout = global.settings.get_value(SCHEMA_KEY);
         this._removeUndone = false;
+
+        let undoInfo = null;
+        let currentLocation = this._getIconLocation(id);
+        if (currentLocation) {
+            undoInfo = { id: id,
+                         folderId: currentLocation[0],
+                         insertId: currentLocation[1] };
+        }
 
         this.repositionIcon(id, null, null);
 
@@ -172,7 +200,7 @@ const IconGridLayout = new Lang.Class({
             Main.overview.setMessage(_("%s has been deleted").format(info.get_name()),
                                      { forFeedback: true,
                                        destroyCallback: Lang.bind(this, this._onMessageDestroy, info),
-                                       undoCallback: Lang.bind(this, this._undoRemoveItem, info)
+                                       undoCallback: Lang.bind(this, this._undoRemoveItem, undoInfo)
                                      });
         } else {
             this._onMessageDestroy(info);
@@ -180,8 +208,6 @@ const IconGridLayout = new Lang.Class({
     },
 
     _onMessageDestroy: function(info) {
-        this._previousLayout = null;
-
         if (this._removeUndone) {
             this._removeUndone = false;
             return;
@@ -200,19 +226,18 @@ const IconGridLayout = new Lang.Class({
         }
     },
 
-    _undoRemoveItem: function(info) {
-        if (this._previousLayout) {
-            global.settings.set_value(SCHEMA_KEY, this._previousLayout);
+    _undoRemoveItem: function(undoInfo) {
+        if (undoInfo != null) {
+            this.repositionIcon(undoInfo.id, undoInfo.insertId, undoInfo.folderId);
         }
         this._removeUndone = true;
-        this._previousLayout = null;
     },
 
     listApplications: function() {
         let allApplications = [];
 
-        for (let folderIdx in this._iconTree) {
-            let folder = this._iconTree[folderIdx];
+        for (let folderId in this._iconTree) {
+            let folder = this._iconTree[folderId];
             for (let iconIdx in folder) {
                 let icon = folder[iconIdx];
                 if (!this.iconIsFolder(icon)) {
@@ -224,7 +249,7 @@ const IconGridLayout = new Lang.Class({
         return allApplications;
     },
 
-    repositionIcon: function(id, insertId, newFolder) {
+    repositionIcon: function(id, insertId, newFolderId) {
         let icons;
         let existing = false;
         let isFolder = this.iconIsFolder(id);
@@ -239,9 +264,9 @@ const IconGridLayout = new Lang.Class({
             }
         }
 
-        if (newFolder != null) {
+        if (newFolderId != null) {
             // We're adding or repositioning an icon
-            icons = this._iconTree[newFolder];
+            icons = this._iconTree[newFolderId];
             if (!icons) {
                 // Invalid destination folder
                 return;

commit e0308d773baced95ef75fe97b4b708c33777bddb

iconGrid: fix off-by-one error
This was broken recently in 7cb1a263989f31160e83315e6b9573a45c68fb24.

[endlessm/eos-shell#3302]

js/ui/iconGrid.js
@@ -554,7 +554,7 @@ const IconGrid = new Lang.Class({
         this._allocatedColumns = nColumns;
 
         for (let i = 0; i < children.length; i++) {
-            let rowIndex = Math.floor((i + 1) / this._allocatedColumns);
+            let rowIndex = Math.floor(i / this._allocatedColumns);
             let childBox = this._childAllocation(children[i], box, i);
 
             if (this._rowLimit && rowIndex >= this._rowLimit ||