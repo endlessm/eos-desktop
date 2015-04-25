5e194b73cc949e97cfb4cf970b45f18f05ff000e
Philip Chimento <philip@endlessm.com>
2015-04-24 17:24:05 -0700

    tests: Port session mode test to Jasmine
    This test was not previously run, and escaped Sam's grand switcheroo from
    JSUnit to Jasmine; this resuscitates it, adds the correct typelib paths,
    and puts the correct mock objects in the global object.
    
    [endlessm/eos-sdk#3054]

--- tests/js/ui/sessionModeTest.js
+++ tests/js/ui/sessionModeTest.js
@@ -1,10 +1,34 @@
+const GLib = imports.gi.GLib;
+
+function resetEnvironment() {
+    let settings = jasmine.createSpyObj('settings', [
+        'connect',
+        'get_boolean',
+        'get_value',
+        'reset',
+    ]);
+    settings.get_value.and.returnValue(new GLib.Variant('a{sv}',
+        [new GLib.Variant('{sv}', ['desktop', new GLib.Variant('as', [])])]));
+    settings.get_boolean.and.returnValue(true);
+
+    window.global = {
+        settings: settings,
+    };
+    window._ = (str) => str;
+    window.C_ = (ctx, str) => str;
+}
+resetEnvironment();  // Needed for following import
+
 let sessionModeImport = imports.js.ui.sessionMode;
 
-function testClockIsOnTheRight() {
-    let modes = sessionModeImport._modes;
+describe('The clock', function () {
+    beforeEach(resetEnvironment);
 
-    let rightPanel = modes['gdm']['panel']['right'];
-    
-    assertEquals('dateMenu', rightPanel[0]);
-}
+    it('is on the right', function () {
+        let modes = sessionModeImport._modes;
 
+        let rightPanel = modes['gdm']['panel']['right'];
+
+        expect(rightPanel[0]).toEqual('dateMenu');
+    });
+});
