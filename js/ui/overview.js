28ddad01fe600465eb00a283d2d64070d47d4b05
Cosimo Cecchi <cosimo@endlessm.com>
2015-06-22 17:56:00 -0700

    overview: more fixes for background changes

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -246,7 +246,7 @@ const Overview = new Lang.Class({
             if (child == this._viewsClone) {
                 continue;
             }
-            Tweener.addTween(child._delegate,
+            Tweener.addTween(child,
                              { brightness: 1.0,
                                time: SHADE_ANIMATION_TIME,
                                transition: 'easeOutQuad' });
@@ -260,7 +260,7 @@ const Overview = new Lang.Class({
             if (child == this._viewsClone) {
                 continue;
             }
-            Tweener.addTween(child._delegate,
+            Tweener.addTween(child,
                              { brightness: 0.7,
                                time: SHADE_ANIMATION_TIME,
                                transition: 'easeOutQuad' });
