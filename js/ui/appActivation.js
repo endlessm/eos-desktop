d1b3a264757ae840c708ef7b120acfc9687834de
Cosimo Cecchi <cosimo@endlessm.com>
2015-07-10 18:11:32 -0700

    Revert "appActivation: use the basename heuristic to lookup launched app"
    This reverts commit 2143a2debb6fecc7c6e4803613a59b6b1037abca.
    
    Conflicts:
    	js/ui/appActivation.js
    
    [endlessm/eos-shell#5298]

--- js/ui/appActivation.js
+++ js/ui/appActivation.js
@@ -454,7 +454,7 @@ const DesktopAppClient = new Lang.Class({
 
         let launchedByShell = (sender_name == Gio.DBus.session.get_unique_name());
         let desktopId = GLib.path_get_basename(desktopIdPath.toString());
-        this._lastDesktopApp = Shell.AppSystem.get_default().lookup_heuristic_basename(desktopId);
+        this._lastDesktopApp = Shell.AppSystem.get_default().lookup_app(desktopId);
 
         // Show the splash page if we didn't launch this ourselves, since in that case
         // we already explicitly control when the splash screen should be used
