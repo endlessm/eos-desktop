b65d3f28e4fa012c35427a489db4f638463bcd62
Michael Catanzaro <mcatanzaro@igalia.com>
2016-03-31 21:23:06 -0500

    screenShield: Do not activate after showing the lightbox
    In upstream GNOME, the screenShield is split into two stages: active
    (screen shield is shown, may or may not be locked) and locked. If the
    shield is locked, then after the screen shield is lifted, the lock
    screen will be visible; otherwise, the shield lifts directly to the
    desktop without requiring unlock.
    
    In Endless, there is no separate screen shield: it is the lock screen.
    So, whereas the upstream code should always activate the screen shield
    after showing the lightbox, we do not want to do so, as that breaks the
    Privacy panel setting that allows the user to specify a set amount of
    time to wait before locking the screen. So... just don't do it. In this
    case, it's desired and expected that the user return directly to the
    desktop.
    https://phabricator.endlessm.com/T6221

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -108,7 +108,6 @@ const ScreenShield = new Lang.Class({
                                                { inhibitEvents: true,
                                                  fadeInTime: STANDARD_FADE_TIME,
                                                  fadeFactor: 1 });
-        this._lightbox.connect('shown', Lang.bind(this, this._onLightboxShown));
 
         this.idleMonitor = Meta.IdleMonitor.get_core();
         this._cursorTracker = Meta.CursorTracker.get_for_screen(global.screen);
@@ -239,10 +238,6 @@ const ScreenShield = new Lang.Class({
             this.deactivate(false);
             return;
         }
-    },
-
-    _onLightboxShown: function() {
-        this.activate(false);
     },
 
     showDialog: function() {
