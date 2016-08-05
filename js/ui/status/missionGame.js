4658cca01ae3dae403e53f044399f37f3ad6f187
Sam Spilsbury <sam@endlessm.com>
2016-08-06 06:40:19 +0800

    missionGame: Subdivide the chatbox into two separate regions
    The region on the left contains the "game status" so to speak
    and the region on the right is for the interactive chatbox.

--- js/ui/status/missionGame.js
+++ js/ui/status/missionGame.js
@@ -8,6 +8,7 @@ const St = imports.gi.St;
 const Showmehow = imports.gi.Showmehow;
 const Signals = imports.signals;
 
+const BoxPointer = imports.ui.boxpointer;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
@@ -25,6 +26,39 @@ function launchLessonAction(lesson) {
     }
 }
 
+/**
+ * createSubMenuMenuItemWithFauxParent
+ *
+ * PopupSubMenuMenuItem makes assumptions about its parent
+ * container. Thankfully the surface area of that assumption
+ * is quite small, so monkey-patch _getTopMenu to just return
+ * the parent and hope for the best.
+ */
+function createSubMenuMenuItemWithFauxParent(name, parent) {
+    let item = new PopupMenu.PopupSubMenuMenuItem(name);
+    item.menu._getTopMenu = Lang.bind(item, function() {
+        return parent;
+    });
+    return item;
+}
+
+
+/**
+ * addSubMenuItemToBox
+ *
+ * Simulates what happens in addMenuItem.
+ */
+function addSubMenuItemToBox(subMenuItem, packingBox, menu) {
+    packingBox.add(subMenuItem.actor);
+    packingBox.add(subMenuItem.menu.actor);
+
+    menu._connectSubMenuSignals(subMenuItem, subMenuItem.menu);
+    menu._connectItemSignals(subMenuItem);
+    subMenuItem._closingId = menu.connect('open-state-changed', function(self, open) {
+        if (!open)
+            subMenuItem.menu.close(BoxPointer.PopupAnimation.FADE);
+    });
+}
 
 const Indicator = new Lang.Class({
     Name: 'MissionGameIndicator',
@@ -34,13 +68,43 @@ const Indicator = new Lang.Class({
         this.parent('folder-drag-accept-symbolic', _("Mission"));
         this.setIcon('folder-drag-accept-symbolic');
 
-        this._adventures = new PopupMenu.PopupSubMenuMenuItem(_("Adventures"));
-        this._spells = new PopupMenu.PopupSubMenuMenuItem(_("Spells"));
+        this._adventures = createSubMenuMenuItemWithFauxParent(_("Adventures"), this.menu);
+        this._spells = createSubMenuMenuItemWithFauxParent(_("Spells"), this.menu);
 
-        this.menu.addMenuItem(new PopupMenu.PopupSwitchMenuItem(_("Mission"), false));
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this.menu.addMenuItem(this._adventures);
-        this.menu.addMenuItem(this._spells);
+        const missionSwitch = new PopupMenu.PopupSwitchMenuItem(_("Mission"), false);
+        const switchesBox = new St.BoxLayout({vertical: true});
+        const hbox = new St.BoxLayout({name: 'switchesArea'});
+
+        this.menu.addActor(hbox);
+        hbox.add(switchesBox);
+        switchesBox.add(missionSwitch.actor);
+        addSubMenuItemToBox(this._adventures, switchesBox, this.menu);
+        addSubMenuItemToBox(this._spells, switchesBox, this.menu);
+
+        const separator = new St.DrawingArea({ style_class: 'calendar-vertical-separator',
+                                               pseudo_class: 'highlighted' });
+        separator.connect('repaint', Lang.bind(this, function(area) {
+            let cr = area.get_context();
+            let themeNode = area.get_theme_node();
+            let [width, height] = area.get_surface_size();
+            let stippleColor = themeNode.get_color('-stipple-color');
+            let stippleWidth = themeNode.get_length('-stipple-width');
+            let x = Math.floor(width/2) + 0.5;
+            cr.moveTo(x, 0);
+            cr.lineTo(x, height);
+            Clutter.cairo_set_source_color(cr, stippleColor);
+            cr.setDash([1, 3], 1); // Hard-code for now
+            cr.setLineWidth(stippleWidth);
+            cr.stroke();
+            cr.$dispose();
+        }));
+        hbox.add(separator);
+
+        const chatboxBox = new St.BoxLayout({ name: 'chatboxArea', vertical: true });
+        hbox.add(chatboxBox);
+
+        const chatboxCheckbox = new PopupMenu.PopupSwitchMenuItem(_("Chatbox"), false);
+        chatboxBox.add(chatboxCheckbox.actor);
 
         let name = "com.endlessm.Showmehow.Service";
         let path = "/com/endlessm/Showmehow/Service";
