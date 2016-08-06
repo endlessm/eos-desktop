7e55ea844752054674b54cbc523a13be000f5443
Sam Spilsbury <sam@endlessm.com>
2016-08-06 11:19:52 +0800

    missionGame: Add chat text area

--- js/ui/status/missionGame.js
+++ js/ui/status/missionGame.js
@@ -4,6 +4,7 @@ const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
+const Pango = imports.gi.Pango;
 const St = imports.gi.St;
 const Showmehow = imports.gi.Showmehow;
 const Signals = imports.signals;
@@ -11,6 +12,8 @@ const Signals = imports.signals;
 const BoxPointer = imports.ui.boxpointer;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+
+const InterfaceSettings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
 
 function launchLessonAction(lesson) {
     return function(event) {
@@ -60,6 +63,129 @@ function addSubMenuItemToBox(subMenuItem, packingBox, menu) {
     });
 }
 
+/**
+ * wrapTextWith
+ *
+ * Naturally wrap lines at constant, appending prefix to each line,
+ * returning an array with the wrapped lines.
+ */
+function wrapTextWith(text, constant, prefix) {
+    let lines = [prefix];
+    let lineIndex = 0;
+    let lineCounter = prefix.length;
+    let textCounter = 0;
+    let lastSpace = 0;
+
+    while (textCounter < text.length) {
+        textCounter++;
+
+        /* Break a line when we either hit a space,
+         * hit a newline, when the line (without spaces)
+         * is too long or when we're at the end of the text */
+        const breakCondition = (text[textCounter] == ' ' ||
+                                text[textCounter] == '\n' ||
+                                textCounter == text.length ||
+                                textCounter - lastSpace >= constant);
+        if (breakCondition) {
+            /* Hit a space, see if we can append this
+             * word, otherwise start a new line */
+            let append = text.slice(lastSpace, textCounter);
+            if (lineCounter + append.length > constant ||
+                text[textCounter] == '\n') {
+                lines.push(prefix);
+                lineIndex++;
+                lineCounter = prefix.length;
+
+                /* Strip leading whitespace in the case of
+                 * new lines */
+                append = append.replace(/^\s+/g, '');
+                lines[lineIndex] = lines[lineIndex] + append;
+            } else {
+                lines[lineIndex] = lines[lineIndex] + append;
+            }
+
+            lineCounter += append.length;
+            lastSpace = textCounter;
+        }
+    }
+
+    return lines;
+}
+
+function copyObject(src) {
+    let dst = {};
+    Lang.copyProperties(src, dst);
+    return dst;
+}
+
+const ScrollingLabel = new Lang.Class({
+    Name: 'ScrollingLabel',
+    Extends: St.Label,
+
+    _init: function(params) {
+        let parentParams = copyObject(params);
+        parentParams.text = '';
+
+        this.parent(parentParams);
+        this._text = params.text;
+        this._textIndex = 0;
+        this._scrollTimer = 0;
+        this.complete = false;
+    },
+    start: function(scrollView) {
+        /* Avoid double-start */
+        if (this._scrollTimer) {
+            return;
+        }
+
+        /* Immediately display the first character so that
+         * scrolling to the bottom will work */
+        this._textIndex++;
+        this.set_text(this._text.slice(0, this._textIndex));
+
+        /* Add a timeout to gradually display all this text */
+        if (this._textIndex < this._text.length) {
+            this._scrollTimer = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 10, Lang.bind(this, function() {
+                this._textIndex++;
+                this.set_text(this._text.slice(0, this._textIndex));
+
+                const incomplete = this._textIndex < this._text.length;
+                if (!incomplete) {
+                    this.complete = true;
+                    this.emit('finished-scrolling');
+                }
+
+                /* Running this on a timer every time is not
+                 * ideal, but I haven't found a better way
+                 * ensure the view is reliably always
+                 * scrolled */
+                scrollView();
+
+                return incomplete;
+            }));
+        }
+    },
+    fastForward: function() {
+        this._textIndex = this._text.length - 1;
+    }
+});
+Signals.addSignalMethods(ScrollingLabel.prototype);
+
+
+/**
+ * _addLabelToChatboxArea
+ *
+ * Helper function to add a ScrollableLabel to a chatbox results area.
+ */
+function _addLabelToChatboxArea(label, chatboxResultsArea) {
+    chatboxResultsArea.add(label);
+
+    /* Start scrolling the label */
+    label.start(function() {
+        chatboxResultsArea.vadjustment.set_value(Number.MAX_VALUE);
+    });
+}
+
 const Indicator = new Lang.Class({
     Name: 'MissionGameIndicator',
     Extends: PanelMenu.SystemStatusButton,
@@ -103,11 +229,35 @@ const Indicator = new Lang.Class({
         const chatboxBox = new St.BoxLayout({ name: 'chatboxArea', vertical: true });
         hbox.add(chatboxBox);
 
-        const chatboxCheckbox = new PopupMenu.PopupSwitchMenuItem(_("Chatbox"), false);
-        chatboxBox.add(chatboxCheckbox.actor);
+        this._chatboxLabels = [];
+        this._chatboxResultsScrollView = new St.ScrollView({ overlay_scrollbars: true });
+        this._chatboxResultsArea = new St.BoxLayout({ name: 'chatboxResultsArea', vertical: true });
+        const chatboxEntryArea = new St.BoxLayout({ name: 'chatboxEntryArea' });
+
+        this._chatboxResultsScrollView.add_actor(this._chatboxResultsArea);
+
+        chatboxBox.add(this._chatboxResultsScrollView, { expand: true });
+        chatboxBox.add(chatboxEntryArea);
+
+        chatboxEntryArea.add(new St.Label({ text: '> ' }));
+
+        this._chatboxEntry = new St.Entry({ can_focus: true });
+        chatboxEntryArea.add(this._chatboxEntry, { expand: true });
+
+        chatboxBox.set_size(400, 450);
+
+        const fontName = InterfaceSettings.get_string('monospace-font-name');
+        const fontDesc = Pango.FontDescription.from_string(fontName);
+        chatboxBox.style =
+            'font-size: ' + fontDesc.get_size() / 1024. +
+            (fontDesc.get_size_is_absolute() ? 'px' : 'pt') + ';'
+            + 'font-family: "' + fontDesc.get_family() + '";';
 
         let name = "com.endlessm.Showmehow.Service";
         let path = "/com/endlessm/Showmehow/Service";
+
+        /* Null-initialise service for now, but we'll set it later */
+        this._service = null;
 
         Showmehow.ServiceProxy.new_for_bus(Gio.BusType.SESSION, 0, name, path, null,
                                            Lang.bind(this, function(source, result) {
@@ -140,5 +290,48 @@ const Indicator = new Lang.Class({
                 }
             }));
         }));
+
+        this._chatboxEntry.clutter_text.connect('activate', Lang.bind(this, function(entry, event) {
+            const text = entry.get_text()
+            if (text === '') {
+                return true;
+            }
+
+            this._evaluate(text);
+            entry.set_text('');
+            return true;
+        }));
     },
+    _evaluate: function(text) {
+        let wrappedText = text;
+        wrappedText.replace('\n', ' ');
+        wrappedText.replace(/^\s+/g, '').replace(/\s+$/g, '');
+
+        const wrapConstant = 42;
+
+        wrapTextWith(wrappedText, wrapConstant, '> ').forEach(Lang.bind(this, function(line) {
+            this._pushLabelToChatboxResultsArea(new ScrollingLabel({
+                text: line
+            }));
+        }));
+    },
+    _pushLabelToChatboxResultsArea: function(label) {
+        /* Push immediately if we're the first or if the last one
+         * has finished scrolling */
+        const lastLabel = this._chatboxLabels.length === 0 ? null : this._chatboxLabels[this._chatboxLabels.length - 1];
+        const immediate = !lastLabel || lastLabel.complete;
+
+        /* Immediately put the label in this._chatboxLabels, however
+         * this does not mean it will be immediately added to the
+         * area. */
+        this._chatboxLabels.push(label);
+
+        if (immediate) {
+            _addLabelToChatboxArea(label, this._chatboxResultsArea);
+        } else {
+            lastLabel.connect('finished-scrolling', Lang.bind(this, function() {
+               _addLabelToChatboxArea(label, this._chatboxResultsArea);
+            }));
+        }
+    }
 });
