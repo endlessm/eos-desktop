6d4e115e1e0d0fbc6e3d534cf4bcfce11a7de4f6
Sam Spilsbury <sam@endlessm.com>
2016-09-06 22:30:27 +0800

    windowManager: Remove empty line

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -1629,7 +1629,6 @@ const WindowManager = new Lang.Class({
         if (!window)
             return;
 
-
         let actor = window.get_compositor_private();
         let effect = actor.get_effect('endless-wobbly');
 
