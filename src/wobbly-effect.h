eae9d4ee423192ed1c09feb60ae9413ae2bb26b2
Cosimo Cecchi <cosimo@endlessm.com>
2016-09-08 14:35:21 -0700

    wobbly-effect: add license to header file too

--- src/wobbly-effect.h
+++ src/wobbly-effect.h
@@ -1,7 +1,22 @@
 /*
  * wobbly-effect.h
  *
- * Copyright (c) Endless Mobile Inc.
+ * Copyright © 2013-2016 Endless Mobile, Inc.
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * licence or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Sam Spilsbury <sam@endlessm.com>
  */
 
 #ifndef ENDLESS_SHELL_FX_WOBBLY_H
