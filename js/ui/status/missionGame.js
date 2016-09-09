ab1b452263efa0a06afdb48588344564ea4b6e84
Sam Spilsbury <sam@endlessm.com>
2016-09-09 23:07:04 +0800

    missionGame: Turn createSeparator into a class

--- js/ui/status/missionGame.js
+++ js/ui/status/missionGame.js
@@ -547,27 +547,33 @@ const MissionToolbox = new Lang.Class({
     }
 });
 
-function createSeparator() {
-    let separator = new St.DrawingArea({ style_class: 'calendar-vertical-separator',
-                                           pseudo_class: 'highlighted' });
-    separator.connect('repaint', Lang.bind(this, function(area) {
-        let cr = area.get_context();
-        let themeNode = area.get_theme_node();
-        let [width, height] = area.get_surface_size();
-        let stippleColor = themeNode.get_color('-stipple-color');
-        let stippleWidth = themeNode.get_length('-stipple-width');
-        let x = Math.floor(width/2) + 0.5;
-        cr.moveTo(x, 0);
-        cr.lineTo(x, height);
-        Clutter.cairo_set_source_color(cr, stippleColor);
-        cr.setDash([1, 3], 1); // Hard-code for now
-        cr.setLineWidth(stippleWidth);
-        cr.stroke();
-        cr.$dispose();
-    }));
+const PanelMenuSeparator = new Lang.Class({
+    Name: 'PanelMenuSeparator',
+    Extends: St.DrawingArea,
 
-    return separator;
-}
+    _init: function(params) {
+        this.parent(Params.parse(params, {
+            style_class: 'calendar-vertical-separator',
+            pseudo_class: 'highlighted'
+        }));
+
+        this.connect('repaint', Lang.bind(this, function(area) {
+            let cr = area.get_context();
+            let themeNode = area.get_theme_node();
+            let [width, height] = area.get_surface_size();
+            let stippleColor = themeNode.get_color('-stipple-color');
+            let stippleWidth = themeNode.get_length('-stipple-width');
+            let x = Math.floor(width/2) + 0.5;
+            cr.moveTo(x, 0);
+            cr.lineTo(x, height);
+            Clutter.cairo_set_source_color(cr, stippleColor);
+            cr.setDash([1, 3], 1); // Hard-code for now
+            cr.setLineWidth(stippleWidth);
+            cr.stroke();
+            cr.$dispose();
+        }));
+    }
+});
 
 /* Separate indicators option */
 const MissionGameToolboxIndicator = new Lang.Class({
@@ -635,7 +641,7 @@ const MissionGameIndicator = new Lang.Class({
 
         /* Add toolbox, separator, chatbox */
         hbox.add(new MissionToolbox({}, this.menu, this._service));
-        hbox.add(createSeparator());
+        hbox.add(new PanelMenuSeparator());
         hbox.add(new MissionChatbox({}, this._service));
 
         this.menu.addActor(hbox);
