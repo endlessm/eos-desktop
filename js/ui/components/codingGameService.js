f28e329a9201c2672a596546a5e0e11275df0a1a
Sam Spilsbury <sam@endlessm.com>
2016-09-19 17:07:01 +0800

    coding: Connect to coding-game-service and listen for events there
    We now listen for move-window and stop-moving windows and have
    some architecture in place to register and unregister signals
    when appropriate to do so.

--- js/ui/components/codingGameService.js
+++ js/ui/components/codingGameService.js
@@ -1,9 +1,10 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Meta = imports.gi.Meta;
-const Showmehow = imports.gi.Showmehow;
+const CodingGameService = imports.gi.CodingGameService;
 
 const CodingChatboxTextService = new Lang.Class({
     Name: 'CodingChatboxTextService',
@@ -11,42 +12,141 @@ const CodingChatboxTextService = new Lang.Class({
     enable: function() {
         // Connect to the service
         try {
-            this._service = Showmehow.ServiceProxy.new_for_bus_sync(
+            this._service = CodingGameService.CodingGameServiceProxy.new_for_bus_sync(
                 Gio.BusType.SESSION, 0,
-                'com.endlessm.Showmehow.Service',
-                '/com/endlessm/Showmehow/Service', null);
+                'com.endlessm.CodingGameService.Service',
+                '/com/endlessm/CodingGameService/Service', null);
         } catch (e) {
-            logError(e, 'Error occurred in creating ShowmehowServiceProxy');
+            logError(e, 'Error occurred in creating CodingGameServiceProxy');
             return;
         }
 
-        // It doesn't seem possible to return an array of strings here, looks like it has to be an array
-        // of tuples (which contain strings).
-        this._listenForLessonsId =
-            this._service.connect('listening-for-lesson-events', Lang.bind(this, function(proxy, interestingEvents) {
-                let events = interestingEvents.deep_unpack().map(function(i) {
-                    return i[0];
-                });
+        let eventHandlers = {
+            'move-window': Lang.bind(this, this._waitForNextWindowMove),
+            'stop-moving-windows': Lang.bind(this, this._waitForNoWindowsToMove)
+        };
 
-                if (events.indexOf('window-moved') !== -1)
-                    this._waitForNextWindowMove();
+        this._signalHandlers = {};
+        this._listenForEventsId =
+            this._service.connect('listen-for-event', Lang.bind(this, function(proxy, name) {
+                if (eventHandlers[name]) {
+                    this._disconnectHandlersFor(name);
+                    eventHandlers[name]();
+                }
             }));
+        this._stopListeningForEventsId =
+            this._service.connect('stop-listening-for', Lang.bind(this, function(proxy, name) {
+                // In this case, just disconnect any known event handlers, don't
+                // need any custom logic here
+                this._disconnectHandlersFor(name);
+            }));
+
+        this._service.call_currently_listening_for_events(null, Lang.bind(this, function(source, result) {
+            let success, returnValue;
+
+            try {
+                [success, returnValue] = this._service.call_currently_listening_for_events_finish(result);
+            } catch (e) {
+                logError(e);
+                return;
+            }
+
+            returnValue.deep_unpack().map(Lang.bind(this, function(name) {
+                if (eventHandlers[name]) {
+                    this._disconnectHandlersFor(name);
+                    eventHandlers[name]();
+                }
+            }));
+        }));
     },
 
     disable: function() {
-        if (this._listenForLessonsId > 0) {
-            this._service.disconnect(this._listenForLessonsId);
-            this._listenForLessonsId = 0;
+        Object.keys(this._signalHandlers, Lang.bind(this, function(key) {
+            this._disconnectHandlersFor(key);
+        }));
+    },
+
+    _disconnectHandlersFor: function(name) {
+        name = name.replace(/\-/g, '_');
+        if (!this._signalHandlers[name]) {
+            return;
         }
+
+        this._signalHandlers[name].forEach(function(handler) {
+            if (handler.object) {
+                handler.object.disconnect(handler.connection);
+            } else {
+                // Asumme this is a GSource
+                GLib.source_remove(handler.connection);
+            }
+        });
+        this._signalHandlers[name] = [];
     },
 
     _waitForNextWindowMove: function() {
-        let grabBeginId = global.display.connect('grab-op-begin', function(display, screen, window, op) {
+        let wmCallback = Lang.bind(this, function(display, screen, window, op) {
             if (window && op == Meta.GrabOp.MOVING) {
-                this._service.call_lesson_event('window-moved', null, null);
-                global.display.disconnect(grabBeginId);
+                this._service.call_external_event('move-window', null, null);
+                this._disconnectHandlersFor('move-window');
             }
         });
+        this._signalHandlers.move_window = [{
+            object: global.display,
+            connection: global.display.connect('grab-op-begin', wmCallback)
+        }];
+    },
+
+    _waitForNoWindowsToMove: function() {
+        // Here we create a timeout of 5000ms and for a case where the user is
+        // no longer moving windows around.
+        //
+        // Create the timeout on grab-op-end and if the user doesn't do something
+        // that causes grab-op-begin within that time, call the ExternalEvent
+        // method with 'stop-moving-window'
+
+        let timeoutCb = Lang.bind(this, function() {
+            this._disconnectHandlersFor('stop-moving-windows');
+            this._service.call_external_event('stop-moving-windows', null, null);
+            return false;
+        });
+
+        let grabOpBeginCb = Lang.bind(this, function(display, screen, window, op) {
+            if (window && op === Meta.GrabOp.MOVING) {
+                // Started moving a window again - disconnect this signal handler
+                // and connect a signal handler to wait for windows to stop moving.
+                this._disconnectHandlersFor('stop-moving-windows');
+                this._signalHandlers.stop_moving_windows = [
+                    {
+                        object: global.display,
+                        connection: global.display.connect('grab-op-end', grabOpEndCb)
+                    }
+                ];
+            }
+        });
+
+        let grabOpEndCb = Lang.bind(this, function(display, screen, window, op) {
+            if (window && op === Meta.GrabOp.MOVING) {
+                // Window has finished moving - disconnect this signal handler
+                // and connect a signal handler for both this timeout and
+                // grab-op-begin in its place.
+                this._disconnectHandlersFor('stop-moving-windows');
+                this._signalHandlers.stop_moving_windows = [
+                    {
+                        object: global.display,
+                        connection: global.display.connect('grab-op-begin', grabOpBeginCb)
+                    },
+                    {
+                        object: null,
+                        connection: GLib.timeout_add(GLib.PRIORITY_DEFAULT, 5000, timeoutCb)
+                    }
+                ];
+            }
+        });
+
+        this._signalHandlers.stop_moving_windows = [{
+            object: global.display,
+            connection: global.display.connect('grab-op-end', grabOpEndCb)
+        }];
     }
 });
 const Component = CodingChatboxTextService;
