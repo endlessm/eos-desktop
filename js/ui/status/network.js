endlessm/eos-desktop

commit a336e78fa2c2e4a3de7b836b57b1d0cf8893b80e

network: Fix the AP strength indicator never getting updated
I intended to make a few code cleanups, but I apparently forgot
to hook up _updateAccessPoint. Merge it with _activeApChanged,
which is where the notify::active-access-point signal is actually
hooked up to.

https://bugzilla.gnome.org/show_bug.cgi?id=704670

js/ui/status/network.js
@@ -782,6 +782,11 @@ const NMDeviceWireless = new Lang.Class({
             this._apRemovedId = 0;
         }
 
+        if (this._strengthChangedId) {
+            this._activeAccessPoint.disconnect(this._strengthChangedId);
+            this._strengthChangedId = 0;
+        }
+
         this.parent();
     },
 
@@ -811,18 +816,31 @@ const NMDeviceWireless = new Lang.Class({
     },
 
     _activeApChanged: function() {
-        this._activeNetwork = null;
+        let ap = this._device.active_access_point;
+        if (this._activeAccessPoint == ap)
+            return;
 
-        let activeAp = this._device.active_access_point;
+        if (this._activeAccessPoint) {
+            this._activeAccessPoint.disconnect(this._strengthChangedId);
+            this._strengthChangedId = 0;
+        }
 
-        if (activeAp) {
+        this._activeAccessPoint = ap;
+
+        if (this._activeAccessPoint) {
+            this._strengthChangedId = this._activeAccessPoint.connect('notify::strength',
+                                                                      Lang.bind(this, this._strengthChanged));
+
             let res = this._findExistingNetwork(activeAp);
-
             if (res != null)
                 this._activeNetwork = this._networks[res.network];
         }
 
-        // we don't refresh the view here, _activeConnectionChanged will
+        this.emit('icon-changed');
+    },
+
+    _strengthChanged: function() {
+        this.emit('icon-changed');
     },
 
     _getApSecurityType: function(accessPoint) {
@@ -1222,30 +1240,6 @@ const NMDeviceWireless = new Lang.Class({
 
             this._createNetworkItem(network, j + activeOffset);
         }
-    },
-
-    _updateAccessPoint: function() {
-        let ap = this._device.active_access_point;
-        if (this._activeAccessPoint == ap)
-            return;
-
-        if (this._activeAccessPoint) {
-            this._activeAccessPoint.disconnect(this._strengthChangedId);
-            this._strengthChangedId = 0;
-        }
-
-        this._activeAccessPoint = ap;
-
-        if (this._activeAccessPoint) {
-            this._strengthChangedId = this._activeAccessPoint.connect('notify::strength',
-                                                                      Lang.bind(this, this._strengthChanged));
-        }
-
-        this._syncStatusLabel();
-    },
-
-    _strengthChanged: function() {
-        this.emit('icon-changed');
     },
 
     getIndicatorIcon: function() {

commit 15ce95bffe50e0accf5ef88b58c0337bca8f8304

network: use proper variable name
Fallout from a336e78fa2c2e4a3de7b836b57b1d0cf8893b80e.

https://phabricator.endlessm.com/T4444

js/ui/status/network.js
@@ -816,7 +816,7 @@ const NMDeviceWireless = new Lang.Class({
             this._strengthChangedId = this._activeAccessPoint.connect('notify::strength',
                                                                       Lang.bind(this, this._strengthChanged));
 
-            let res = this._findExistingNetwork(activeAp);
+            let res = this._findExistingNetwork(this._activeAccessPoint);
             if (res != null)
                 this._activeNetwork = this._networks[res.network];
         }