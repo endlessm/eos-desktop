21cde2bc18b62918f63d671b4ba817d1e094be1b
Sam Spilsbury <sam@endlessm.com>
2017-04-14 15:04:13 +0800

    sessionMode: Also show discovery feed in coding session too

--- js/ui/sessionMode.js
+++ js/ui/sessionMode.js
@@ -114,7 +114,7 @@ const _modes = {
         unlockDialog: imports.ui.unlockDialog.UnlockDialog,
         components: ['networkAgent', 'polkitAgent',
                      'keyring', 'autorunManager', 'automountManager',
-                     'updaterManager', 'socialBar', 'appStore', 'codingManager', 'codingGameService'],
+                     'updaterManager', 'socialBar', 'appStore', 'codingManager', 'codingGameService', 'discoveryFeed'],
         panel: {
             left: ['userMenu', 'panelSeparator', 'appIcons'],
             right: ['dateMenu', 'a11y', 'keyboard', 'volume', 'bluetooth',
